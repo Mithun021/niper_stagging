27d97b7e18809925ed6562978d42da4f2f58c205
Mithun021 <mithunkr79038@gmail.com>
2024-12-06 21:26:27 +0530

    emp project

--- app/Views/admin/employee/employee-projects.php
+++ app/Views/admin/employee/employee-projects.php
@@ -1,6 +1,11 @@
 <?= $this->extend("admin/layouts/master") ?>
 <?= $this->section("body-content"); ?>
 
+<?php
+    use App\Models\Employee_model;
+    $employee_model = new Employee_model();
+?>
+
 <div class="row">
     <div class="col-lg-12">
         <div class="card">
