002cb12523fc803b9627f10afff1d7137d13b726
Mithun021 <mithunkr79038@gmail.com>
2024-12-06 20:22:43 +0530

    emp

--- app/Models/Employee_experience_model.php
+++ app/Models/Employee_experience_model.php
@@ -22,7 +22,7 @@
             if($id != null){
                 $result = $this->where('id',$id)->first();
             }else{
-                $result = $this->findAll();
+                $result = $this->orderBy('emplyee_id','asc')->findAll();
             }
             return $result;
         }
