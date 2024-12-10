4084c7a47f137bd37b0c8c656a0eb461e24d1ef6
Mithun021 <mithunkr79038@gmail.com>
2024-12-10 16:33:12 +0530

    quick link

--- app/Views/admin/quick-link.php
+++ app/Views/admin/quick-link.php
@@ -17,7 +17,7 @@
                         echo session()->getFlashdata('status');
                     }
                 ?>
-                <form action="<?= base_url() ?>admin/contact" method="post" enctype="multipart/form-data">
+                <form action="<?= base_url() ?>admin/quick-link" method="post" enctype="multipart/form-data">
                     <div class="form-group">
                         <span for="">Title<span class="text-danger">*</span></span>
                         <input type="text" class="form-control form-control-sm" name="quicklink_title" required>
