2fa14e59c61363e79c25e9f9c5858a2540ac4df5
Mithun021 <mithunkr79038@gmail.com>
2024-12-11 12:42:16 +0530

    clone emp

--- app/Views/admin/employee/employee-projects.php
+++ app/Views/admin/employee/employee-projects.php
@@ -149,7 +149,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Upload Employee Experience Data</h5>
+        <h5 class="modal-title">Upload Employee Projects Data</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
