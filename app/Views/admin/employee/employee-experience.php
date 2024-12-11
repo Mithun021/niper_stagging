66a98bebb9869c653a006b194be2a0267b33b24a
Mithun021 <mithunkr79038@gmail.com>
2024-12-11 12:21:28 +0530

    clone emp

--- app/Views/admin/employee/employee-experience.php
+++ app/Views/admin/employee/employee-experience.php
@@ -15,7 +15,7 @@
 <div class="row">
     <div class="col-lg-12">
         <div class="card">
-            <div class="card-header d-flex justify-content-between">
+            <div class="card-header d-flex justify-content-between align-items-center">
                 <h4 class="card-title m-0">Add <?= $title; ?></h4>
                 <div>
                     <button type="button" class="btn btn-sm btn-danger">Export Sample</button>
