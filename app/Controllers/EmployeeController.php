913b4d1666440c5af3e07528a74633cc93f8e027
Mithun021 <mithunkr79038@gmail.com>
2024-12-12 13:04:52 +0530

    add clone emp

--- app/Controllers/EmployeeController.php
+++ app/Controllers/EmployeeController.php
@@ -105,10 +105,10 @@ use App\Models\Employee_publication_model;
                         'upload_by' =>  $loggeduserId,
                     ];
     
-                    echo "<pre>";print_r($data);
-                    //$result = $employee_experience_model->add($data);
+                    // echo "<pre>";print_r($data);
+                    $result = $employee_experience_model->add($data);
                 }
-                die;
+                // die;
                 if ($result === true) {
                     return redirect()->to('admin/employee-experience')->with('msg','<div class="alert alert-success" role="alert"> Data Add Successful </div>');
                 } else {
