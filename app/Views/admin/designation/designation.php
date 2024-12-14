67ee21626514c37d929d8ff6db35fc06817d43c5
Mithun021 <mithunkr79038@gmail.com>
2024-12-14 12:48:10 +0530

    emp designation upldate

--- app/Views/admin/designation/designation.php
+++ app/Views/admin/designation/designation.php
@@ -6,7 +6,7 @@
 </style>
 <!-- start page title -->
 <div class="row">
-    <div class="col-lg-5">
+    <div class="col-lg-4">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Add <?= $title ?> </h4>
@@ -40,7 +40,7 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-7">
+    <div class="col-lg-8">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0"><?= $title ?> List</h4>
