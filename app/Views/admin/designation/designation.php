c01c3fda705af7158336e06c475380ad70809a70
Mithun021 <mithunkr79038@gmail.com>
2024-12-14 12:48:43 +0530

    emp designation upldate

--- app/Views/admin/designation/designation.php
+++ app/Views/admin/designation/designation.php
@@ -68,7 +68,6 @@
                             <td><?= $value['created_at'] ?></td>
                             <td>
                                 <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
-                                    <a href="#" class="btn btn-dark waves-effect waves-light"><i class="far fa-eye"></i></a>
                                     <a href="#" class="btn btn-primary waves-effect waves-light"><i class="fas fa-pen"></i></a>
                                     <a href="#" class="btn btn-danger waves-effect waves-light"><i class="far fa-trash-alt"></i></a>
                                 </div>
