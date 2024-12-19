4a0d3b20791ecbe99177561a3502ea70e27d7849
Mithun021 <mithunkr79038@gmail.com>
2024-12-19 15:20:09 +0530

    Emp Add Charge

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,7 +35,7 @@ $routes->group('admin',['filter'=>'adminLogin'], static function($routes){
     $routes->match(['get','post'],'employee-publication', 'EmployeeController::employee_publication');
     $routes->match(['get','post'],'employee-awards', 'EmployeeController::employee_awards');
     $routes->match(['get','post'],'employee-charge', 'EmployeeController::employee_charge');
-    $routes->get('admin/get-employee-designations/(:num)', 'EmployeeController::getEmployeeDesignations/$1');
+    $routes->get('get-employee-designations/(:num)', 'EmployeeController::getEmployeeDesignations/$1');
 
 
     $routes->match(['get','post'],'images', 'AdminControllers::images');
