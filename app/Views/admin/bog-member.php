736ee9fb8f1c7939f82c7a9bdc069794b26d038d
Mithun021 <mithunkr79038@gmail.com>
2024-12-21 17:02:04 +0530

    Bog Member

--- app/Views/admin/bog-member.php
+++ app/Views/admin/bog-member.php
@@ -1,5 +1,9 @@
 <?= $this->extend("admin/layouts/master") ?>
 <?= $this->section("body-content"); ?>
+<?php
+    use App\Models\Employee_model;
+    $employee_model = new Employee_model();
+?>
 
 <div class="row">
     <div class="col-lg-4">
@@ -62,11 +66,25 @@
                                 <td>Affiliation</td>
                                 <td>Designation</td>
                                 <td>Term Year</td>
+                                <td>Upload by</td>
                                 <td>Actions</td>
                             </tr>
                         </thead>
                         <tbody>
-                            <!-- Data to be dynamically populated -->
+                            <?php foreach ($bog_members as $key => $value) { ?>
+                                <tr>
+                                    <td><?= $key + 1 ?></td>
+                                    <td><?= $value['member_name'] ?></td>
+                                    <td><?= $value['affiliation'] ?></td>
+                                    <td><?= $value['designation'] ?></td>
+                                    <td><?= $value['term_start_year'] . ' - ' . $value['term_end_year'] ?></td>
+                                    <td><?php $emp = $employee_model->get($value['upload_by']); echo $emp['first_name']." ".$emp['middle_name']." ".$emp['last_name']  ?></td>
+                                    <td>
+                                        <a href="<?= base_url() ?>admin/bog-member/<?= $value['id'] ?>" class="btn btn-sm btn-primary">Edit</a>
+                                        <a href="<?= base_url() ?>admin/bog-member/delete/<?= $value['id'] ?>" class="btn btn-sm btn-danger">Delete</a>
+                                    </td>
+                                </tr>
+                            <?php } ?>
                         </tbody>
                     </table>
                 </div>
