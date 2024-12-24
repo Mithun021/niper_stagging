f07e9ea1ba9bc2972244ec9095b675d496537c8f
Mithun021 <mithunkr79038@gmail.com>
2024-12-24 13:48:14 +0530

    Feedback

--- app/Views/admin/testimonial.php
+++ app/Views/admin/testimonial.php
@@ -9,7 +9,7 @@
 </style>
 <!-- start page title -->
 <div class="row">
-    <div class="col-lg-5">
+    <div class="col-lg-4">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Testimonial </h4>
@@ -45,7 +45,7 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-7">
+    <div class="col-lg-8">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Testimonial List</h4>
