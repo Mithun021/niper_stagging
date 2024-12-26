bd0acc8597c12bcf16f2b9a741530b0109bd33a1
Mithun021 <mithunkr79038@gmail.com>
2024-12-26 16:37:51 +0530

    Download form

--- app/Views/admin/download-forms.php
+++ app/Views/admin/download-forms.php
@@ -76,7 +76,7 @@
                                         <td><?= $key + 1 ?></td>
                                         <td>
                                         <?php if (!empty($value['upload_file']) && file_exists('public/admin/uploads/forms/' . $value['upload_file'])): ?>
-                                            <a href="<?= base_url() ?>public/admin/uploads/forms/<?= $value['upload_file'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/uploads/forms/<?= $value['upload_file'] ?>" alt="" height="30px"></a>
+                                            <a href="<?= base_url() ?>public/admin/uploads/forms/<?= $value['upload_file'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/assets/images/folder.png" alt="" height="30px"></a>
                                         <?php else: ?>
                                             <img src="<?= base_url() ?>public/admin/uploads/forms/invalid_image.png" alt="" height="40px">
                                         <?php endif; ?>
