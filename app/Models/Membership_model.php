b8057a07d512eb81084e27d926bcda45ed783824
Mithun021 <mithunkr79038@gmail.com>
2024-12-26 15:31:41 +0530

    Dynamic Program

--- app/Models/Membership_model.php
+++ app/Models/Membership_model.php
@@ -22,7 +22,7 @@
             if($id != null){
                 $result = $this->where('id',$id)->first();
             }else{
-                $result = $this->orderBy('name','asc')->findAll();
+                $result = $this->orderBy('title','asc')->findAll();
             }
             return $result;
         }
