ae0b3586e7f804280aa8d3b34b3a0e691bce2316
Mithun021 <mithunkr79038@gmail.com>
2024-12-27 11:26:19 +0530

    Placement Details

--- app/Models/Placement_details_model.php
+++ app/Models/Placement_details_model.php
@@ -22,7 +22,7 @@
             if($id != null){
                 $result = $this->where('id',$id)->first();
             }else{
-                $result = $this->orderBy('title','asc')->findAll();
+                $result = $this->orderBy('department_id','asc')->findAll();
             }
             return $result;
         }
