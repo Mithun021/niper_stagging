811927ee55b12737466601e9d1c8df486902df3c
Mithun021 <mithunkr79038@gmail.com>
2024-12-27 12:11:27 +0530

    Instrument

--- app/Views/admin/instrument-facility.php
+++ app/Views/admin/instrument-facility.php
@@ -1,6 +1,10 @@
 <!-- app/Views/recruiterdetails_form.php -->
 <?= $this->extend("admin/layouts/master") ?>
 <?= $this->section("body-content"); ?>
+<?php
+    use App\Models\Employee_model;
+    $employee_model = new Employee_model();
+?>
 
 <div class="row">
     <!-- Form Section for Adding <?= $title ?> -->
