cdd782a65578ea0e86fb4547abeeba6114812a1f
Mithun021 <mithunkr79038@gmail.com>
2024-12-28 19:16:08 +0530

    Menu Bar

--- app/Views/admin/layouts/footer.php
+++ app/Views/admin/layouts/footer.php
@@ -88,7 +88,7 @@
                 $.ajax({
                     url: "<?= base_url() ?>admin/save_menu_heading_sort_order",
                     method: "GET",
-                    data: { sortedData: sortedData },
+                    data: JSON.stringify({ sortedData: sortedData }),
                     success: function (response) {
                         console.log("Menu heading sort order saved successfully.", response);
                     },
@@ -117,7 +117,7 @@
                 $.ajax({
                     url: "<?= base_url() ?>admin/save_menu_page_sort_order",
                     method: "GET",
-                    data: { sortedData: sortedPagesData },
+                    data: JSON.stringify({ sortedData: sortedData }),
                     success: function (response) {
                         console.log("Menu page sort order saved successfully.", response);
                     },
