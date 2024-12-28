f061768f5c2d83c5362da1591204ec12b9bed7d0
Mithun021 <mithunkr79038@gmail.com>
2024-12-28 13:31:41 +0530

    Menu Bar

--- app/Views/admin/menu.php
+++ app/Views/admin/menu.php
@@ -78,12 +78,16 @@
                 </div>
                 <div id="collapse<?= $collapse ?>" class="collapse" aria-labelledby="heading<?= $heading ?>" data-parent="#accordion">
                     <div class="card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                        non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
-                        tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil
-                        anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                        excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt
-                        you probably haven't heard of them accusamus labore sustainable VHS.
+                        <form action="<?= base_url() ?>admin/menu-heading" method="post">
+                            <input type="text" name="menu_id" value="<?= $value['id'] ?>">
+                            <div class="form-group">
+                                <label for="heading">Heading</label>
+                                <input type="text" name="heading" id="heading" class="form-control form-control-sm" required>   
+                            </div>
+                            <div class="form-group"></div>
+                                <input type="submit" value="Save Heading" class="btn btn-primary">
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div> <!-- end card-->
