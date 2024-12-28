135189652c3748e2f1d90cdbd20cd97fe09141eb
Mithun021 <mithunkr79038@gmail.com>
2024-12-28 13:02:27 +0530

    Menu Bar

--- app/Models/Menu_name_model.php
+++ app/Models/Menu_name_model.php
@@ -22,7 +22,7 @@
             if($id != null){
                 $result = $this->where('id',$id)->first();
             }else{
-                $result = $this->orderBy('instrument_id','asc')->findAll();
+                $result = $this->orderBy('name','asc')->findAll();
             }
             return $result;
         }
