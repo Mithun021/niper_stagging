7ec2332d9d41387d900b3f30d9a0a732a0784d61
Mithun021 <mithunkr79038@gmail.com>
2024-12-28 14:05:55 +0530

    Menu Bar

--- app/Models/Menu_heading_model.php
+++ app/Models/Menu_heading_model.php
@@ -22,7 +22,7 @@
             if($id != null){
                 $result = $this->where('id',$id)->first();
             }else{
-                $result = $this->orderBy('name','asc')->findAll();
+                $result = $this->orderBy('menu_id','asc')->orderBy('heading','asc')->findAll();
             }
             return $result;
         }
