a746524f63e7634c46da902ded80452b3cbdfa25
Mithun021 <mithunkr79038@gmail.com>
2024-12-28 16:18:08 +0530

    Menu Bar

--- app/Views/admin/menu.php
+++ app/Views/admin/menu.php
@@ -152,7 +152,7 @@
                                             <table class="mytable">
                                             <?php foreach ($page_name as $key2 => $pages) { ?>
                                                 <tr>
-                                                    <td width="85%"><?= $pages['page_name'] ?></td>
+                                                    <td width="85%"><a href="<?= base_url() ?><?= $pages['page_name'] ?>" target="_blank"><?= $pages['page_name'] ?></a></td>
                                                     <td><i class="fa fa-trash" aria-hidden="true"></i></td>
                                                 </tr>
                                             <?php } ?>
