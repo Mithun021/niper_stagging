273f69d0b6c6ab781a1385f12a46ed5dd70e419a
Mithun021 <mithunkr79038@gmail.com>
2024-12-30 13:30:51 +0530

    Menu Bar

--- app/Models/Menu_pages_model.php
+++ app/Models/Menu_pages_model.php
@@ -22,7 +22,7 @@
             if($id != null){
                 $result = $this->where('id',$id)->first();
             }else{
-                $result = $this->orderBy('name','asc')->findAll();
+                $result = $this->orderBy('page_sort_list','asc')->findAll();
             }
             return $result;
         }
