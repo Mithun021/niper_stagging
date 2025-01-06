bc5413d8a840e086b57b3da19f9e7ff92ad7d98e
Mithun021 <mithunkr79038@gmail.com>
2025-01-06 15:00:04 +0530

    Events Management

--- app/Views/admin/events/event-post.php
+++ app/Views/admin/events/event-post.php
@@ -164,7 +164,7 @@
             </div>
             <div class="card-body">
                 <div class="table-responsive">
-                <table class="table table-striped table-hover" id="basic-datatable" style="width: 150%;">
+                <table class="table table-striped table-hover" id="basic-datatable" style="width: 120%;">
                     <thead>
                         <tr>
                             <td>SN</td>
