dba29a0879a1378178f99985c210c3c82501e7b9
Mithun021 <mithunkr79038@gmail.com>
2025-01-07 12:38:48 +0530

    CommitteeController

--- app/Views/admin/patent/patent-details.php
+++ app/Views/admin/patent/patent-details.php
@@ -1,6 +1,9 @@
 <?= $this->extend("admin/layouts/master") ?>
-
 <?= $this->section("body-content"); ?>
+<?php
+    use App\Models\Employee_model;
+    $employee_model = new Employee_model();
+?>
 
 <!-- Patent Details Form -->
 <div class="row">
@@ -75,17 +78,42 @@
                         <thead>
                             <tr>
                                 <td>SN</td>
+                                <td>File</td>
                                 <td>Title</td>
                                 <td>Number</td>
                                 <td>Patent Date</td>
                                 <td>Status</td>
+                                <td>Emp. ID</td>
                                 <td>Uploaded by</td>
-                                <td>Create at</td>
                                 <td>Action</td>
                             </tr>
                         </thead>
                         <tbody>
-                            <!-- Table data goes here -->
+                            <?php foreach ($patent as $key => $value) { ?>
+                                <tr>
+                                    <td><?= $key+1 ?></td>
+                                    <td>
+                                    <?php if (!empty($value['upload_file']) && file_exists('public/admin/uploads/patent/' . $value['upload_file'])): ?>
+                                        <a href="<?= base_url() ?>public/admin/uploads/patent/<?= $value['upload_file'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/assets/images/folder.png" alt="" height="30px"></a>
+                                    <?php else: ?>
+                                        <img src="<?= base_url() ?>public/admin/uploads/patent/invalid_image.png" alt="" height="40px">
+                                    <?php endif; ?>
+                                    </td>
+                                    <td><?= $value['patent_title'] ?></td>
+                                    <td><?= $value['patent_number'] ?></td>
+                                    <td><?= $value['patent_date'] ?></td>
+                                    <td><?= $value['status'] == 0 ? '<span class="badge badge-danger badge-pill">Draft</span>' : '<span class="badge badge-success badge-pill">Active</span>' ?></td>
+                                    <td><?php $emp = $employee_model->get($value['employee_id']); echo $emp['first_name']." ".$emp['middle_name']." ".$emp['last_name']  ?></td>
+                                    <td><?php $emp = $employee_model->get($value['upload_by']); echo $emp['first_name']." ".$emp['middle_name']." ".$emp['last_name']  ?></td>
+                                    <td>
+                                        <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
+                                            <a href="#" class="btn btn-dark waves-effect waves-light"><i class="far fa-eye"></i></a>
+                                            <a href="#" class="btn btn-primary waves-effect waves-light"><i class="fas fa-pen"></i></a>
+                                            <a href="#" class="btn btn-danger waves-effect waves-light"><i class="far fa-trash-alt"></i></a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            <?php } ?>
                         </tbody>
                     </table>
                 </div>
