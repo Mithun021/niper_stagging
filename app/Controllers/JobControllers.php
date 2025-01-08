8b21fb4ad2893daa1abb8e004ad9e9697b954539
Mithun021 <mithunkr79038@gmail.com>
2025-01-08 15:06:04 +0530

    job and result

--- app/Controllers/JobControllers.php
+++ app/Controllers/JobControllers.php
@@ -2,14 +2,26 @@
 
 namespace App\Controllers;
 
+use App\Models\Job_category_model;
+
 class JobControllers extends BaseController
 {
     public function job_category(){
+        $job_category_model = new Job_category_model();
         $data = ['title' => 'Job Category'];
         if ($this->request->is("get")) {
+            $data['job_category'] = $job_category_model->get();
             return view('admin/jobs/job-category',$data);
         }else if ($this->request->is("post")) {
-
+            $data =[
+                'name' => $this->request->getPost('category_name'),
+            ];
+            $result = $job_category_model->add($data);
+            if ($result === true) {
+                return redirect()->to('admin/job-category')->with('status','<div class="alert alert-success" role="alert"> Data Add Successful </div>');
+            } else {
+                return redirect()->to('admin/job-category')->with('status','<div class="alert alert-danger" role="alert"> '.$result.' </div>');
+            }
         }
     }
     public function job_details(){
@@ -25,7 +37,7 @@ class JobControllers extends BaseController
         if ($this->request->is("get")) {
             return view('admin/jobs/job-result',$data);
         }else if ($this->request->is("post")) {
-
+            
         }
     }
 }
--- app/Views/admin/jobs/job-category.php
+++ app/Views/admin/jobs/job-category.php
@@ -1,5 +1,4 @@
 <?= $this->extend("admin/layouts/master") ?>
-
 <?=  $this->section("body-content"); ?>
 <style>
     
@@ -17,12 +16,11 @@
                         echo session()->getFlashdata('status');
                     }
                 ?>
-                <form id="noticeBoardForm">
+                <form method="post" action="<?= base_url() ?>admin/job-category">
                     <div class="form-group">
                         <span for="">Category Name<span class="text-danger">*</span></span>
-                        <input type="text" class="form-control form-control-sm" name="notice_title">
+                        <input type="text" class="form-control form-control-sm" name="category_name" required>
                     </div>
-
                     <button type="submit" class="btn btn-sm btn-primary" id="submitBtn">Save</button>
                     
                 </form>
@@ -41,11 +39,24 @@
                         <tr>
                             <td>SN</td>
                             <td>Category Name</td>
+                            <td>Upload by</td>
                             <td>Action</td>
                         </tr>
                     </thead>
                     <tbody>
-                    
+                    <?php foreach($job_category as $key => $value){ ?>
+                        <tr>
+                            <td><?= $key+1 ?></td>
+                            <td><?= $value['name'] ?></td>
+                            <td><?= $value['created_at'] ?></td>
+                            <td>
+                                <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
+                                    <a href="#" class="btn btn-primary waves-effect waves-light"><i class="fas fa-pen"></i></a>
+                                    <a href="#" class="btn btn-danger waves-effect waves-light"><i class="far fa-trash-alt"></i></a>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php } ?>
                     </tbody>
                 </table>
                 </div>
