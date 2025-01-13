c19ba097e3f5caac23070b6e411cadb84b2600b1
Mithun021 <mithunkr79038@gmail.com>
2025-01-13 16:39:06 +0530

    Academic Module

--- app/Views/admin/academics/accouncement.php
+++ app/Views/admin/academics/accouncement.php
@@ -1,8 +1,8 @@
 <?= $this->extend("admin/layouts/master") ?>
 
-<?=  $this->section("body-content"); ?>
+<?= $this->section("body-content"); ?>
 <style>
-    
+
 </style>
 <!-- start page title -->
 <div class="row">
@@ -13,11 +13,15 @@
             </div>
             <div class="card-body p-2">
                 <?php
-                    if(session()->getFlashdata('status')){
-                        echo session()->getFlashdata('status');
-                    }
+                if (session()->getFlashdata('status')) {
+                    echo session()->getFlashdata('status');
+                }
                 ?>
                 <form id="noticeBoardForm">
+                    <div class="form-group">
+                        <span for="">Anouncement Date<span class="text-danger">*</span></span>
+                        <input type="date" id="annoncement_date" name="annoncement_date" class="from-control form-control-sm" required>
+                    </div>
                     <div class="form-group">
                         <span for="">Title<span class="text-danger">*</span></span>
                         <textarea id="editor2" name="content"></textarea>
@@ -31,9 +35,25 @@
                         <span for="">Desription</span>
                         <textarea id="editor" name="content"></textarea>
                     </div>
+                    <div class="form-group">
+                        <span>Announcement Status</span>
+                        <select name="status" id="status" class="form-control form-control-sm">
+                            <option value="1">Publish</option>
+                            <option value="2">Archive</option>
+                            <option value="0">Draft</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <span>Marquee Status</span>
+                        <select name="status" id="status" class="form-control form-control-sm">
+                            <option value="1">Active</option>
+                            <option value="0">Inactive</option>
+                        </select>
+                    </div>
 
                     <button type="submit" class="btn btn-sm btn-primary" id="submitBtn">Save</button>
-                    
+
                 </form>
             </div>
         </div>
@@ -45,20 +65,20 @@
             </div>
             <div class="card-body p-2">
                 <div class="table-responsive">
-                <table class="table table-striped table-hover" id="basic-datatable">
-                    <thead>
-                        <tr>
-                            <td>SN</td>
-                            <td>Title</td>
-                            <td>Files</td>
-                            <td>Create at</td>
-                            <td>Action</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    
-                    </tbody>
-                </table>
+                    <table class="table table-striped table-hover" id="basic-datatable">
+                        <thead>
+                            <tr>
+                                <td>SN</td>
+                                <td>Title</td>
+                                <td>Files</td>
+                                <td>Create at</td>
+                                <td>Action</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
