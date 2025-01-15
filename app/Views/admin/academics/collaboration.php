f54037fd2fdcf9d6c9190646962b56f7a0a0655c
Mithun021 <mithunkr79038@gmail.com>
2025-01-15 10:39:22 +0530

    collaboration

--- app/Views/admin/academics/collaboration.php
+++ app/Views/admin/academics/collaboration.php
@@ -15,66 +15,76 @@
 
                 <!-- Form Start -->
                 <form action="<?= base_url() ?>admin/collaboration" method="post" enctype="multipart/form-data">
-                <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <span for="Collabtitle">Collaboration Title:</span>
-                        <input type="text" name="Collabtitle" id="Collabtitle" class="form-control form-control-sm" required>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <span for="Collabtitle">Collaboration Title:</span>
+                                <input type="text" name="Collabtitle" id="Collabtitle" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <span for="Collabinstitutename">Institution Name:</span>
+                                <input type="text" name="Collabinstitutename" id="Collabinstitutename" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <span for="Mediatitle">Description:</span>
+                                <textarea id="editor" name="description"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <span for="Collaborationdatetime">Collaboration Date and Time:</span>
+                                <input type="date" name="Collaborationdatetime" id="Collaborationdatetime" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <span for="Collabinstitutelogo">Institution Logo(PNG,JPG,JPEG):</span>
+                                <input type="file" name="Collabinstitutelogo" id="Collabinstitutelogo" class="form-control form-control-sm" accept=".png,.jpg,.jpeg" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <span for="Collabinstituelink">Institution Link:</span>
+                                <input type="url" name="Collabinstituelink" id="Collabinstituelink" class="form-control form-control-sm">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <span for="Collabfileupload">Collaboration File Upload(PDF):</span>
+                                <input type="file" name="Collabfileupload" id="Collabfileupload" class="form-control form-control-sm" accept=".pdf">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <span for="Collabenddatetime">Collaboration tenure :</span>
+                                <input type="datetime-local" name="Collabtenure" id="Collabtenure" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <span for="Collabstatus">Collaboration Status:</span>
+                                <select name="Collabstatus" id="Collabstatus" class="form-control form-control-sm" required>
+                                    <option value="active">Active</option>
+                                    <option value="expired">Expired</option>
+                                    <option value="renewed">Renewed</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <!-- Renewal Date field, initially hidden -->
+                        <div class="col-md-6" id="renewalDateField" style="display: none;">
+                            <div class="form-group">
+                                <label for="RenewalDate">Renewal Date:</label>
+                                <input type="date" name="RenewalDate" id="RenewalDate" class="form-control form-control-sm">
+                            </div>
+                        </div>
+
                     </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <span for="Collabinstitutename">Institution Name:</span>
-                        <input type="text" name="Collabinstitutename" id="Collabinstitutename" class="form-control form-control-sm" required>
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <span for="Mediatitle">Description:</span>
-                        <textarea id="editor" name="content"></textarea>
-                    </div>
-                </div>
-                
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <span for="Collaborationdatetime">Collaboration Date and Time:</span>
-                        <input type="datetime-local" name="Collaborationdatetime" id="Collaborationdatetime" class="form-control form-control-sm" required>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <span for="Collabinstitutelogo">Institution Logo:</span>
-                        <input type="file" name="Collabinstitutelogo" id="Collabinstitutelogo" class="form-control form-control-sm" accept="image/*">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <span for="Collabinstituelink">Institution Link:</span>
-                        <input type="url" name="Collabinstituelink" id="Collabinstituelink" class="form-control form-control-sm">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <span for="Collabfileupload">Collaboration File Upload:</span>
-                        <input type="file" name="Collabfileupload" id="Collabfileupload" class="form-control form-control-sm" accept=".pdf,.docx,.zip">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <span for="Collabenddatetime">Collaboration End Date and Time:</span>
-                        <input type="datetime-local" name="Collabenddatetime" id="Collabenddatetime" class="form-control form-control-sm" required>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <span for="Collabstatus">Status:</span>
-                        <select name="Collabstatus" id="Collabstatus" class="form-control form-control-sm" required>
-                            <option value="1">Active</option>
-                            <option value="0">Inactive</option>
-                        </select>
-                    </div>
-                </div>
-                </div>
 
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
@@ -111,5 +121,17 @@
         </div>
     </div>
 </div>
+
+<script>
+    document.getElementById('Collabstatus').addEventListener('change', function() {
+        var renewalDateField = document.getElementById('renewalDateField');
+        
+        if (this.value === 'renewed') {
+            renewalDateField.style.display = 'block'; // Show Renewal Date field
+        } else {
+            renewalDateField.style.display = 'none'; // Hide Renewal Date field
+        }
+    });
+</script>
 
 <?= $this->endSection() ?>
