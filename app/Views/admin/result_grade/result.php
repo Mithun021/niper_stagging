d395892e7307de7d02f5af781a240e50f32d3b28
Mithun021 <mithunkr79038@gmail.com>
2025-01-17 13:57:02 +0530

    Convocation

--- app/Views/admin/result_grade/result.php
+++ app/Views/admin/result_grade/result.php
@@ -4,7 +4,7 @@
 
 <div class="row">
     <!-- Form Section for Adding Result Details -->
-    <div class="col-lg-4">
+    <div class="col-lg-12">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Add Result Details</h4>
@@ -16,67 +16,77 @@
 
                 <!-- Form Start -->
                 <form action="<?= base_url() ?>admin/result" method="post" enctype="multipart/form-data">
-                    <!-- Result Description -->
-                    <div class="form-group">
-                        <span for="resultdesc">Result Description:<span class="text-danger">*</span></span>
-                        <textarea name="resultdesc" id="editor" class="form-control form-control-sm" required ></textarea>
-                    </div>
 
-                    <div class="form-group">
-                        <span for="Deptid">Department ID:<span class="text-danger">*</span></span>
-                        <select name="Deptid" id="Deptid" class="form-control form-control-sm" required >
-                            <option value="">Select Deparrtment</option>
-                        <?php foreach ($department as $key => $value) { ?>
-                            <option value="<?= $value['id'] ?>"><?= $value['name'] ?></option>
-                        <?php } ?>
-                        </select>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <!-- Result Description -->
+                            <div class="form-group">
+                                <span for="resultdesc">Result Description:<span class="text-danger">*</span></span>
+                                <textarea name="resultdesc" id="editor" class="form-control form-control-sm" required></textarea>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Deptid">Department ID:<span class="text-danger">*</span></span>
+                                <select name="Deptid" id="Deptid" class="form-control form-control-sm" required>
+                                    <option value="">Select Deparrtment</option>
+                                    <?php foreach ($department as $key => $value) { ?>
+                                        <option value="<?= $value['id'] ?>"><?= $value['name'] ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Progid">Program ID:<span class="text-danger">*</span></span>
+                                <select name="Progid" id="Progid" class="form-control form-control-sm" required>
+                                    <option value="">Select Program</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Convnumber">Academic Session Start Year:<span class="text-danger">*</span></span>
+                                <input type="number" name="academic_start_year" id="academic_start_year" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Convnumber">Academic Session End Year:<span class="text-danger">*</span></span>
+                                <input type="number" name="academic_end_year" id="academic_end_year" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-6"><!-- Semester -->
+                            <div class="form-group">
+                                <span for="semester">Semester:<span class="text-danger">*</span></span>
+                                <select name="semester" id="semester" class="form-control form-control-sm" required>
+                                    <option value="I">I</option>
+                                    <option value="II">II</option>
+                                    <option value="III">III</option>
+                                    <option value="IV">IV</option>
+                                    <option value="V">V</option>
+                                    <option value="VI">VI</option>
+                                    <option value="VII">IVI</option>
+                                    <option value="VIII">VIII</option>
+                                    <option value="IX">IX</option>
+                                    <option value="X">X</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Convnumber">Result Notification Date:<span class="text-danger">*</span></span>
+                                <input type="date" name="notification_date" id="academic_end_year" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <!-- Result File Upload -->
+                            <div class="form-group mt-3">
+                                <span for="Resultfileupload">Upload Result File(.pdf):<span class="text-danger">*</span></span>
+                                <input type="file" name="file_upload" id="file_upload" class="form-control form-control-sm" accept=".pdf" required>
+                            </div>
+                        </div>
                     </div>
-
-                    <div class="form-group">
-                        <span for="Progid">Program ID:<span class="text-danger">*</span></span>
-                        <select name="Progid" id="Progid" class="form-control form-control-sm" required >
-                            <option value="">Select Program</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <span for="Convnumber">Academic Session Start Year:<span class="text-danger">*</span></span>
-                        <input type="number" name="academic_start_year" id="academic_start_year" class="form-control form-control-sm" required>
-                    </div>
-
-                    <div class="form-group">
-                        <span for="Convnumber">Academic Session End Year:<span class="text-danger">*</span></span>
-                        <input type="number" name="academic_end_year" id="academic_end_year" class="form-control form-control-sm" required>
-                    </div>
-
-                    <!-- Semester -->
-                    <div class="form-group">
-                        <span for="semester">Semester:<span class="text-danger">*</span></span>
-                        <select name="semester" id="semester" class="form-control form-control-sm" required>
-                            <option value="I">I</option>
-                            <option value="II">II</option>
-                            <option value="III">III</option>
-                            <option value="IV">IV</option>
-                            <option value="V">V</option>
-                            <option value="VI">VI</option>
-                            <option value="VII">IVI</option>
-                            <option value="VIII">VIII</option>
-                            <option value="IX">IX</option>
-                            <option value="X">X</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <span for="Convnumber">Result Notification Date:<span class="text-danger">*</span></span>
-                        <input type="date" name="notification_date" id="academic_end_year" class="form-control form-control-sm" required>
-                    </div>
-
-                    <!-- Result File Upload -->
-                    <div class="form-group mt-3">
-                        <span for="Resultfileupload">Upload Result File(.pdf):<span class="text-danger">*</span></span>
-                        <input type="file" name="file_upload" id="file_upload" class="form-control form-control-sm" accept=".pdf" required>
-                    </div>
-
                     <!-- Submit Button -->
                     <button type="submit" class="btn btn-primary mt-4">Submit</button>
                 </form>
@@ -85,7 +95,7 @@
     </div>
 
     <!-- Table Section to Display Existing Result Details (Optional) -->
-    <div class="col-lg-8">
+    <div class="col-lg-12">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Result Details List</h4>
@@ -116,21 +126,23 @@
 <script src="<?= base_url() ?>public/admin/assets/js/jquery.min.js"></script>
 
 <script>
-    $(document).ready(function(){
+    $(document).ready(function() {
         // Fetch Programs based on Department
-        $('#Deptid').change(function(){
+        $('#Deptid').change(function() {
             var dept_id = $(this).val();
             $.ajax({
                 url: '<?= base_url() ?>fetch-programs',
                 type: 'post',
-                data: {dept_id: dept_id},
-                success: function(response){
+                data: {
+                    dept_id: dept_id
+                },
+                success: function(response) {
                     // console.log(response);
                     let dataList = $('#Progid');
                     dataList.empty();
                     dataList.append('<option value="">Select Program</option>');
                     $.each(response, function(index, item) {
-                        dataList.append('<option value="'+ item.program_id +'">'+ item.program_name + "("+ item.batch_start + "-" + item.batch_end +")" +'</option>');
+                        dataList.append('<option value="' + item.program_id + '">' + item.program_name + "(" + item.batch_start + "-" + item.batch_end + ")" + '</option>');
                     });
                 }
             });
