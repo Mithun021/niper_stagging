686322705f82c297e59d8220da874defbfb5790e
Mithun021 <mithunkr79038@gmail.com>
2025-01-17 13:03:33 +0530

    Convocation

--- app/Views/admin/convocation/convocation.php
+++ app/Views/admin/convocation/convocation.php
@@ -76,7 +76,7 @@ $employee_model = new Employee_model();
                                 <td><?= ++$key ?></td>
                                 <td>
                                     <?php if (!empty($value['upload_file']) && file_exists('public/admin/uploads/convocation/' . $value['upload_file'])): ?>
-                                        <a href="<?= base_url() ?>public/admin/uploads/convocation/<?= $value['upload_file'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/uploads/convocation/<?= $value['upload_file'] ?>" alt="" height="30px"></a>
+                                        <a href="<?= base_url() ?>public/admin/uploads/convocation/<?= $value['upload_file'] ?>" target="_blank"><img src="<?= base_url() ?>public/assets/images/pdf.png" alt="" height="30px"></a>
                                     <?php else: ?>
                                         <img src="<?= base_url() ?>public/admin/uploads/convocation/invalid_image.png" alt="" height="40px">
                                     <?php endif; ?>
