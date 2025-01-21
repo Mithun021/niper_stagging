3a7c6846326174330a73eef1b23f0b3f9d12c11b
Mithun021 <mithunkr79038@gmail.com>
2025-01-21 11:35:56 +0530

    Updates files

--- app/Views/admin/committee/committee-details.php
+++ app/Views/admin/committee/committee-details.php
@@ -3,13 +3,14 @@
 <?= $this->section("body-content"); ?>
 <?php
 
-    use App\Models\Committee_model;
-    use App\Models\Employee_model;
-    $committee_model = new Committee_model();
-    $employee_model = new Employee_model();
+use App\Models\Committee_model;
+use App\Models\Employee_model;
+
+$committee_model = new Committee_model();
+$employee_model = new Employee_model();
 ?>
 <div class="row">
-    <div class="col-lg-4">
+    <div class="col-lg-12">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Add <?= $title; ?></h4>
@@ -22,35 +23,58 @@
 
                 <!-- Form Start -->
                 <form action="<?= base_url() ?>admin/committee-details" method="post" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <span for="Committeetitle">Committee Title:</span>
-                        <input type="text" name="Committeetitle" id="Committeetitle" class="form-control form-control-sm" required>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Committeetitle">Committee Title:</span>
+                                <input type="text" name="Committeetitle" id="Committeetitle" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Committeetitle">Parent Committee:</span>
+                                <select name="subcommitteeid" id="subcommitteeid" class="form-control form-control-sm">
+                                    <option value="">--Select Committee--</option>
+                                    <?php foreach ($committee as $key => $value) { ?>
+                                        <option value="<?= $value['id'] ?>"><?= $value['title'] ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Committeedesc">Committee Description:</span>
+                                <textarea id="editor" name="description"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Committeefileupload">File Upload(pdf):</span>
+                                <input type="file" name="Committeefileupload" id="Committeefileupload" class="form-control form-control-sm" accept=".pdf">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Committeestatus">Status:</span>
+                                <select class="form-control form-control-sm" name="committee_status" required>
+                                    <option value="0">Draft</option>
+                                    <option value="1">Active</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span>Start Date</span>
+                                <input type="date" name="comm_start_date" id="comm_start_date" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span>End Date</span>
+                                <input type="date" name="comm_end_date" id="comm_end_date" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <span for="Committeetitle">Parent Committee:</span>
-                        <select name="subcommitteeid" id="subcommitteeid" class="form-control form-control-sm">
-                            <option value="">--Select Committee--</option>
-                        <?php foreach ($committee as $key => $value) { ?>
-                            <option value="<?= $value['id'] ?>"><?= $value['title'] ?></option>
-                        <?php } ?>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <span for="Committeedesc">Committee Description:</span>
-                        <textarea id="editor" name="description"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <span for="Committeefileupload">File Upload(pdf):</span>
-                        <input type="file" name="Committeefileupload" id="Committeefileupload" class="form-control form-control-sm" accept=".pdf">
-                    </div>
-                    <div class="form-group">
-                        <span for="Committeestatus">Status:</span>
-                        <select class="form-control form-control-sm" name="committee_status" required>
-                            <option value="0">Draft</option>
-                            <option value="1">Active</option>
-                        </select>
-                    </div>
-
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
@@ -58,7 +82,7 @@
     </div>
 
     <!-- Display Committees in a Table -->
-    <div class="col-lg-8">
+    <div class="col-lg-12">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0"><?= $title; ?> List</h4>
@@ -78,30 +102,32 @@
                             </tr>
                         </thead>
                         <tbody>
-                        <?php foreach ($committee as $key => $value) { ?>
-                            <tr>
-                                <td><?= $key+1 ?></td>
-                                <td>
-                                    <?php if (!empty($value['upload_file']) && file_exists('public/admin/uploads/committee/' . $value['upload_file'])): ?>
-                                        <a href="<?= base_url() ?>public/admin/uploads/committee/<?= $value['upload_file'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/assets/images/pdf.png" alt="" height="30px"></a>
-                                    <?php else: ?>
-                                        <img src="<?= base_url() ?>public/admin/uploads/committee/invalid_image.png" alt="" height="40px">
-                                    <?php endif; ?>
-                                </td>
-                                <td><?= $value['title'] ?></td>
-                                <td><?php $sub_comm = $committee_model->get($value['sub_committee']); echo isset($sub_comm['title']) && !empty($sub_comm['title']) ? $sub_comm['title'] : '__'; ?></td>
-                                <td><?= ($value['status'] == "0") ? "<span class='badge badge-danger badge-pill'>Draft</span>" : (($value['status'] == "1") ? "<span class='badge badge-success badge-pill'>Active</span>" : "") ?></td>
-                                
-                                <td><?php $emp = $employee_model->get($value['upload_by']); echo $emp['first_name']." ".$emp['middle_name']." ".$emp['last_name'] ?></td>
-                                <td>
-                                    <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
-                                        <a href="#" class="btn btn-dark waves-effect waves-light"><i class="far fa-eye"></i></a>
-                                        <a href="#" class="btn btn-primary waves-effect waves-light"><i class="fas fa-pen"></i></a>
-                                        <a href="#" class="btn btn-danger waves-effect waves-light"><i class="far fa-trash-alt"></i></a>
-                                    </div>
-                                </td>
-                            </tr>
-                        <?php } ?>  
+                            <?php foreach ($committee as $key => $value) { ?>
+                                <tr>
+                                    <td><?= $key + 1 ?></td>
+                                    <td>
+                                        <?php if (!empty($value['upload_file']) && file_exists('public/admin/uploads/committee/' . $value['upload_file'])): ?>
+                                            <a href="<?= base_url() ?>public/admin/uploads/committee/<?= $value['upload_file'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/assets/images/pdf.png" alt="" height="30px"></a>
+                                        <?php else: ?>
+                                            <img src="<?= base_url() ?>public/admin/uploads/committee/invalid_image.png" alt="" height="40px">
+                                        <?php endif; ?>
+                                    </td>
+                                    <td><?= $value['title'] ?></td>
+                                    <td><?php $sub_comm = $committee_model->get($value['sub_committee']);
+                                        echo isset($sub_comm['title']) && !empty($sub_comm['title']) ? $sub_comm['title'] : '__'; ?></td>
+                                    <td><?= ($value['status'] == "0") ? "<span class='badge badge-danger badge-pill'>Draft</span>" : (($value['status'] == "1") ? "<span class='badge badge-success badge-pill'>Active</span>" : "") ?></td>
+
+                                    <td><?php $emp = $employee_model->get($value['upload_by']);
+                                        echo $emp['first_name'] . " " . $emp['middle_name'] . " " . $emp['last_name'] ?></td>
+                                    <td>
+                                        <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
+                                            <a href="#" class="btn btn-dark waves-effect waves-light"><i class="far fa-eye"></i></a>
+                                            <a href="#" class="btn btn-primary waves-effect waves-light"><i class="fas fa-pen"></i></a>
+                                            <a href="#" class="btn btn-danger waves-effect waves-light"><i class="far fa-trash-alt"></i></a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            <?php } ?>
                         </tbody>
                     </table>
                 </div>
