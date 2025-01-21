617cd960669275d3c40d6b6ae643c44a445bc7f6
Mithun021 <mithunkr79038@gmail.com>
2025-01-21 11:24:15 +0530

    Updates files

--- app/Views/admin/events/event-post.php
+++ app/Views/admin/events/event-post.php
@@ -37,7 +37,7 @@
                         <div class="col-md-6">
                             <div class="form-group">
                                 <span>Event Category<span class="text-danger">*</span></span>
-                                <select name="event_category" id="event_category" class="form-control form-control-sm">
+                                <select name="event_category" id="event_category" class="form-control form-control-sm" required>
                                     <option value="">Select Event Type</option>
                                     <?php foreach ($event_category as $value) { ?>
                                         <option value="<?php echo $value['id']; ?>"><?php echo $value['name']; ?></option>
@@ -49,13 +49,13 @@
                         <div class="col-md-6">
                             <div class="form-group">
                                 <span for="">Registration Link</span>
-                                <input type="url" class="form-control form-control-sm" name="reg_link">
+                                <input type="url" class="form-control form-control-sm" name="reg_link" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <span for="">Event Venue</span>
-                                <input type="text" class="form-control form-control-sm" name="event_venue">
+                                <input type="text" class="form-control form-control-sm" name="event_venue" required>
                             </div>
                         </div>
                         <div class="col-md-6">
@@ -68,7 +68,7 @@
                             <div class="form-group">
                                 <span for="">Event Start and End Date<span class="text-danger">*</span></span>
                                 <div class="input-group">
-                                    <input type="text" class="form-control form-control-sm" name="event_start_date"  placeholder="Start Date" onfocus="(this.type='date')" onblur="(this.type='text')">
+                                    <input type="text" class="form-control form-control-sm" name="event_start_date"  placeholder="Start Date" onfocus="(this.type='date')" onblur="(this.type='text')" required>
                                     <input type="text" class="form-control form-control-sm" name="event_end_date"  placeholder="End Date" onfocus="(this.type='date')" onblur="(this.type='text')">
                                 </div>
                             </div>
@@ -77,7 +77,7 @@
                             <div class="form-group">
                                 <span for="">Registration Start Date & Time<span class="text-danger">*</span></span>
                                 <div class="input-group">
-                                    <input type="text" class="form-control form-control-sm" name="reg_start_date"  placeholder="Start Date" onfocus="(this.type='date')" onblur="(this.type='text')">
+                                    <input type="text" class="form-control form-control-sm" name="reg_start_date"  placeholder="Start Date" onfocus="(this.type='date')" onblur="(this.type='text')" required>
                                     <input type="text" class="form-control form-control-sm" name="reg_start_time"  placeholder="Start Time" onfocus="(this.type='time')" onblur="(this.type='text')">
                                 </div>
                             </div>
@@ -100,13 +100,13 @@
                         <div class="col-md-6">
                             <div class="form-group">
                                 <span for="">Participant Seats</span>
-                                <input type="text" class="form-control form-control-sm" name="participant_seats">
+                                <input type="text" class="form-control form-control-sm" name="participant_seats" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <span for="">Participant Eligibility</span>
-                                <input type="text" class="form-control form-control-sm" name="participant_eligibility">
+                                <input type="text" class="form-control form-control-sm" name="participant_eligibility" required>
                             </div>
                         </div>
                         <div class="col-md-6">
