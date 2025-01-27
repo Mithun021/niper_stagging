84f2cf6ef6d6b1dcb86de9839013cb8a3f4c3e95
Mithun021 <mithunkr79038@gmail.com>
2025-01-27 16:15:25 +0530

    Course List

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -112,6 +112,7 @@ $routes->group('admin',['filter'=>'adminLogin'], static function($routes){
     
     $routes->match(['get', 'post'], 'courseList', 'CourseController::courseList');
     $routes->match(['get', 'post'], 'assignCourseList', 'CourseController::assignCourseList');
+    $routes->match(['get', 'post'], 'edit-assign-course/(:num)', 'CourseController::edit_assign_course/$1');
 
     // Export and Import routes
     $routes->post('export_emp_experience_sample', 'EmployeeController::export_emp_experience_sample');
--- app/Controllers/CourseController.php
+++ app/Controllers/CourseController.php
@@ -76,4 +76,45 @@ class CourseController extends BaseController
             }
         }
     }
+    public function edit_assign_course($id){
+        $assign_course_model = new Assign_course_model();
+        $department_model = new Department_model();
+        $courses_model = new Courses_model();
+        $data = ['title' => 'Assign Course', 'assign_course_id' => $id];
+        if ($this->request->is("get")) {
+            $data['department'] = $department_model->get();
+            $data['courses'] = $courses_model->getActiveData();
+            $data['assign_courses'] = $assign_course_model->get();
+            return view('admin/course/edit-assign-course',$data);
+        }else if ($this->request->is("post")) {
+            $sessionData = session()->get('loggedUserData');
+            if ($sessionData) {
+                $loggeduserId = $sessionData['loggeduserId']; 
+            }
+            $course_id = $this->request->getVar('course_id');
+            $credit_score = $this->request->getVar('credit_score');
+            if(empty($course_id)){
+                return redirect()->to('admin/edit-assign-course')->with('status','<div class="alert alert-danger" role="alert"> Please select course </div>');
+            }
+            if(empty($credit_score)){
+                return redirect()->to('admin/edit-assign-course')->with('status','<div class="alert alert-danger" role="alert"> Please select Credits </div>');
+            }
+            foreach ($course_id as $key => $value) {
+                $data =[
+                    'course_id' => $value,
+                    'dept_id' => $this->request->getVar('Deptid'),
+                    'program_id' => $this->request->getVar('Progid'),
+                    'semester' => $this->request->getVar('semester'),
+                    'credits' => $this->request->getVar('credit_score')[$key],
+                    'upload_by' => $loggeduserId
+                ];
+                $result = $assign_course_model->add($data);
+            }
+            if ($result === true) {
+                return redirect()->to('admin/edit-assign-course/'.$id)->with('status','<div class="alert alert-success" role="alert"> Data Add Successful </div>');
+            }else{
+                return redirect()->to('admin/edit-assign-course/'.$id)->with('status','<div class="alert alert-danger" role="alert"> '.$result.' </div>');
+            }
+        }
+    }
 }
