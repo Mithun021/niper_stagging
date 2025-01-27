9ce72f8432707e95226a8b5abb42efd470fa79f4
Mithun021 <mithunkr79038@gmail.com>
2025-01-27 17:59:35 +0530

    Course List

--- app/Views/admin/academics/rules-regulations.php
+++ app/Views/admin/academics/rules-regulations.php
@@ -31,7 +31,7 @@
                         <span for="">Upload file(.pdf)<span class="text-danger">*</span></span>
                         <input type="file" class="form-control form-control-sm" name="upload_file">
                         <?php if (!empty($rules_regulations['upload_file']) && file_exists('public/admin/uploads/rules_regulation/' . $rules_regulations['upload_file'])): ?>
-                            <a href="<?= base_url() ?>public/admin/uploads/rules_regulation/<?= $rules_regulations['upload_file'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/asets/images/pdf.png" alt="" height="30px"></a>
+                            <a href="<?= base_url() ?>public/admin/uploads/rules_regulation/<?= $rules_regulations['upload_file'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/assets/images/pdf.png" alt="" height="30px"></a>
                         <?php else: ?>
                             <img src="<?= base_url() ?>public/admin/uploads/rules_regulation/invalid_image.png" alt="" height="40px">
                         <?php endif; ?>
