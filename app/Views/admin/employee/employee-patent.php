00a77dac2bb79be4e01412c3a5a5c5250373fac4
Mithun021 <mithunkr79038@gmail.com>
2025-02-08 10:54:05 +0530

    Event Member

--- app/Views/admin/employee/employee-patent.php
+++ app/Views/admin/employee/employee-patent.php
@@ -1,11 +1,13 @@
 <?= $this->extend("admin/layouts/master") ?>
-<?=  $this->section("body-content"); ?>
+<?= $this->section("body-content"); ?>
 <?php
-    use App\Models\Employee_model;
-    $employee_model = new Employee_model();
+
+use App\Models\Employee_model;
+
+$employee_model = new Employee_model();
 ?>
 <style>
-    
+
 </style>
 <!-- start page title -->
 <div class="row">
@@ -16,21 +18,55 @@
             </div>
             <div class="card-body">
                 <?php
-                    if(session()->getFlashdata('status')){
-                        echo session()->getFlashdata('status');
-                    }
+                if (session()->getFlashdata('status')) {
+                    echo session()->getFlashdata('status');
+                }
                 ?>
-                <div class="row">
                 <form method="post" action="<?= base_url('admin/organisation-type') ?>">
-                    <div class="form-group">
-                        <span for="">Organization Name<span class="text-danger">*</span></span>
-                        <input type="text" class="form-control form-control-sm" name="organisation_name" required>
-                    </div>
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <span for="">Event Id<span class="text-danger">*</span></span>
+                            <select class="form-control form-control-sm" name="event_id" required>
+                                <option value="">--Select--</option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="">Reason of Awarding<span class="text-danger">*</span></span>
+                            <select class="form-control form-control-sm" name="award_reason" required>
+                                <option value="">--Select--</option>
+                                <option value="Academic">Academic</option>
+                                <option value="Research">Research</option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="">Name of Awarding<span class="text-danger">*</span></span>
+                            <input type="text" name="name_of_awarding" id="" class="form-control form-control-sm">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="">Date of Awarding<span class="text-danger">*</span></span>
+                            <input type="date" name="date_of_awarding" id="" class="form-control form-control-sm">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="">Body name of Awarding<span class="text-danger">*</span></span>
+                            <input type="text" name="body_name_of_awarding" id="" class="form-control form-control-sm">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="">Level<span class="text-danger">*</span></span>
+                            <select class="form-control form-control-sm" name="award_reason" required>
+                                <option value="">--Select--</option>
+                                <option value="National">National</option>
+                                <option value="International">International</option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="">Document Upload(.pdf,.jpg,.jpeg,.png)<span class="text-danger">*</span></span>
+                            <input type="file" name="name_of_awarding" id="" class="form-control form-control-sm" accept=".pdf,.jpg,.jpeg,.png">
+                        </div>
 
-                    <button type="submit" class="btn btn-sm btn-primary" id="submitBtn">Save</button>
-                    
+                        <button type="submit" class="btn btn-sm btn-primary" id="submitBtn">Save</button>
+
+                    </div>
                 </form>
-                </div>
             </div>
         </div>
     </div>
@@ -41,20 +77,20 @@
             </div>
             <div class="card-body">
                 <div class="table-responsive">
-                <table class="table table-striped table-hover" id="basic-datatable">
-                    <thead>
-                        <tr>
-                            <td>SN</td>
-                            <td>Organization Type</td>
-                            <td>Upload By</td>
-                            <td>Upload Date</td>
-                            <td>Action</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    
-                    </tbody>
-                </table>
+                    <table class="table table-striped table-hover" id="basic-datatable">
+                        <thead>
+                            <tr>
+                                <td>SN</td>
+                                <td>Organization Type</td>
+                                <td>Upload By</td>
+                                <td>Upload Date</td>
+                                <td>Action</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
