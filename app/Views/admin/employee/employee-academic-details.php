95a774ee21c6a68d4669179e3a0338638c0bdb4e
Mithun021 <mithunkr79038@gmail.com>
2025-02-08 13:47:02 +0530

    Event Member

--- app/Views/admin/employee/employee-academic-details.php
+++ app/Views/admin/employee/employee-academic-details.php
@@ -104,6 +104,7 @@ $employee_model = new Employee_model();
                                 <td>Subject Studied</td>
                                 <td>Marking Scheme(%/CGPA)</td>
                                 <td>Obtained Result</td>
+                                <td>Passing Year</td>
                                 <td>University (Country)</td>
                                 <td>University (State/UT)</td>
                                 <td>Upload By</td>
