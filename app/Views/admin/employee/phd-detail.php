4b73d2bef31d659ebd00d619538af32a499b31fb
Mithun021 <mithunkr79038@gmail.com>
2025-02-12 12:10:34 +0530

    Event Member

--- app/Views/admin/employee/phd-detail.php
+++ app/Views/admin/employee/phd-detail.php
@@ -34,20 +34,40 @@ $employee_model = new Employee_model();
                             </select>
                         </div>
                         <div class="form-group col-md-6">
-                            <span for="">Examination Type<span class="text-danger">*</span></span>
-                            <input type="text" name="examination_type" id="" class="form-control form-control-sm" required>
+                            <span for="">Name of the Student<span class="text-danger">*</span></span>
+                            <input type="text" name="student_name" id="" class="form-control form-control-sm" required>
                         </div>
                         <div class="form-group col-md-4">
-                            <span for="">Year of Passing</span>
-                            <input type="number" name="passing_year" id="" class="form-control form-control-sm" maxlength="4">
+                            <span for="">Subject Title of the Thesis</span>
+                            <input type="number" name="subject_thesis" id="" class="form-control form-control-sm" maxlength="4">
                         </div>
                         <div class="form-group col-md-4">
-                            <span for="">Conducted By</span>
-                            <input type="text" name="conduct_by" id="" class="form-control form-control-sm">
+                            <span for="">Name of the University</span>
+                            <input type="text" name="university_name" id="" class="form-control form-control-sm">
                         </div>
                         <div class="form-group col-md-4">
-                            <span for="">Roll No</span>
-                            <input type="text" name="roll_no" id="" class="form-control form-control-sm">
+                            <span for="">Department</span>
+                            <input type="text" name="department" id="" class="form-control form-control-sm">
+                        </div>
+                        <div class="form-group col-md-4">
+                            <span for="">University (Country)</span>
+                            <input type="text" name="university_country" id="" class="form-control form-control-sm">
+                        </div>
+                        <div class="form-group col-md-4">
+                            <span for="">Role</span>
+                            <select name="role" id="" class="form-control form-control-sm">
+                                <option value="">--Select--</option>
+                                <option value="Supervisor">Supervisor</option>
+                                <option value="Co-superviser">Co-superviser</option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <span for="">Registration Date</span>
+                            <input type="date" name="registration_date" id="" class="form-control form-control-sm">
+                        </div>
+                        <div class="form-group col-md-4">
+                            <span for="">Document Upload (jpg,jpeg,pdf,png)</span>
+                            <input type="file" name="document_file" id="" class="form-control form-control-sm" accept=".png,.jpg,.jpeg">
                         </div>
                         <div class="form-group col-md-12">
                             <button type="submit" class="btn btn-sm btn-primary" id="submitBtn">Save</button>
