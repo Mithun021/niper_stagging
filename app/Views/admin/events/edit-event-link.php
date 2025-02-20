72e493a2b2f66e3d16c76d07f067ca1e4598f940
Mithun021 <mithunkr79038@gmail.com>
2025-02-20 14:03:00 +0530

    Event update

--- app/Views/admin/events/edit-event-link.php
+++ app/Views/admin/events/edit-event-link.php
@@ -22,7 +22,7 @@
                         echo session()->getFlashdata('status');
                     }
                 ?>
-                <form method="post" action="<?= base_url('admin/editevent-link/'.$event_link_id) ?>">
+                <form method="post" action="<?= base_url('admin/edit-event-link/'.$event_link_id) ?>">
                     <div class="form-group">
                         <span>Event ID:</span>
                         <select name="event_id" class="form-control form-control-sm">
