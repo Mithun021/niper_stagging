e6ed8368feda3e7104bae1d0b82e515db54c4874
Mithun021 <mithunkr79038@gmail.com>
2025-02-24 20:03:21 +0530

    Event update

--- app/Views/admin/department/departments-section.php
+++ app/Views/admin/department/departments-section.php
@@ -10,7 +10,7 @@
 </style>
 <!-- start page title -->
 <div class="row">
-    <div class="col-lg-4">
+    <div class="col-lg-3">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Add <?= $title ?> </h4>
@@ -52,7 +52,7 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-8">
+    <div class="col-lg-9">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0"><?= $title ?> List</h4>
