8fa844e3e08f1ff99f2278a4c981dcb18ac174d0
Mithun021 <mithunkr79038@gmail.com>
2025-02-25 15:05:15 +0530

    Event update

--- app/Controllers/EmployeeController.php
+++ app/Controllers/EmployeeController.php
@@ -41,6 +41,7 @@ use App\Models\Student_model;
                 $data['employee'] = $employee_model->get();
                 return view('admin/employee/employee',$data);
             }else if ($this->request->is("post")) {
+                echo "<pre>";print_r($this->request->getPost('department_id')); die;
                 $sessionData = session()->get('loggedUserData');
                 if ($sessionData) {
                     $loggeduserId = $sessionData['loggeduserId']; 
