0a0098c48bfbc72568ac721589145554d8e0d51a
Mithun021 <mithunkr79038@gmail.com>
2025-02-25 12:15:55 +0530

    Event update

--- app/Models/Course_tought_model.php
+++ app/Models/Course_tought_model.php
@@ -22,7 +22,7 @@
             if($id != null){
                 $result = $this->where('id',$id)->first();
             }else{
-                $result = $this->orderBy('title','asc')->findAll();
+                $result = $this->orderBy('id','asc')->findAll();
             }
             return $result;
         }
