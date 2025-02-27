ad1845f4c2ef2e065921ff30254d9608cd9cbfb9
Mithun021 <mithunkr79038@gmail.com>
2025-02-27 14:57:26 +0530

    Event update

--- app/Views/admin/events/edit-event-fees.php
+++ app/Views/admin/events/edit-event-fees.php
@@ -62,7 +62,7 @@ use App\Models\Events_model;
                         <select class="form-control form-control-sm" name="evtfeesvalue" id="evtfeesvalue" required>
                             <option value="">Select Fee Type</option>
                         <?php foreach ($fee_subcategory_detail as $key => $value) { ?>
-                            <option value="<?= $value['id'] ?>" <?php if($event_fees_detail['evtfeesvalue']== $value['id']){ echo "selected"; }else if($event_fees_detail['evtfeesvalue']== $value['name']){ echo "Selected"; } ?>><?= $value['name'] ?></option>
+                            <option value="<?= $value['id'] ?>" <?php if($event_fees_detail['evtfeesvalue']== $value['id']){ echo "selected"; }else if($event_fees_detail['evtfeesvalue']== "Any Category"){ echo "Selected"; } ?>><?= $value['name'] ?? 'Any Category' ?></option>
                         <?php } ?>
                         </select>
                     </div>
