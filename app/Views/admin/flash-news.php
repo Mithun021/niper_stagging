3641a40b88e53d8996bad91db9d3dc8aa519b134
Mithun021 <mithunkr79038@gmail.com>
2025-02-28 19:31:59 +0530

    Master  update

--- app/Views/admin/flash-news.php
+++ app/Views/admin/flash-news.php
@@ -56,7 +56,7 @@
             </div>
             <div class="card-body">
                 <div class="table-responsive">
-                <table class="table table-striped table-hover" id="basic-datatable">
+                <table class="table table-striped table-hover" id="basic-datatable" style="width: 120%;">
                     <thead>
                         <tr>
                             <td>SN</td>
