ff01b7783b805ae3704ce4653025ec069d273d14
Mithun021 <mithunkr79038@gmail.com>
2025-03-01 12:24:28 +0530

    Master  update

--- app/Views/admin/newsletter.php
+++ app/Views/admin/newsletter.php
@@ -11,7 +11,7 @@ $employee_model = new Employee_model();
 </style>
 <!-- start page title -->
 <div class="row">
-    <div class="col-lg-4">
+    <div class="col-lg-12">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Add <?= $title ?></h4>
@@ -23,23 +23,24 @@ $employee_model = new Employee_model();
                 }
                 ?>
                 <form method="post" action="<?= base_url() ?>admin/newsletter" enctype="multipart/form-data">
-                    <div class="form-group">
+                <div class="row">
+                    <div class="form-group col-lg-6">
                         <span for="">Title<span class="text-danger">*</span></span>
                         <input type="text" class="form-control form-control-sm" name="title">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group col-lg-6">
                         <span for="">Upload Image(JPG,PNG)<span class="text-danger">*</span></span>
                         <input type="file" class="form-control form-control-sm" name="upload_file" accept=".jpg, .png, .jpeg" required>
                     </div>
-                    <div class="form-group">
+                    <div class="form-group col-lg-12">
                         <span for="">Description:</span>
                         <textarea name="description" id="editor" class="form-control form-control-sm"></textarea>
                     </div>
 
-                    <div class="form-group">
+                    <div class="form-group col-lg-6">
                         <span>Start Month and Year</span>
                         <div class="input-group">
-                            <select name="month" class="form-control form-control-sm my-select">
+                            <select name="start_month" class="form-control form-control-sm my-select">
                                 <option value="">--Select Month</option>
                                 <option value="Jan">Jan</option>
                                 <option value="Feb">Feb</option>
@@ -55,7 +56,35 @@ $employee_model = new Employee_model();
                                 <option value="Dec">Dec</option>
                             </select>
 
-                            <select name="year" class="form-control form-control-sm my-select">
+                            <select name="start_year" class="form-control form-control-sm my-select">
+                                <option value="">--Select Year</option>
+                            <?php for ($i = 2000; $i <= date('Y'); $i++) { ?>
+                                <option value="<?= $i ?>"><?= $i ?></option>
+                            <?php } ?>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="form-group col-lg-6">
+                        <span>End Month and Year</span>
+                        <div class="input-group">
+                            <select name="end_month" class="form-control form-control-sm my-select">
+                                <option value="">--Select Month</option>
+                                <option value="Jan">Jan</option>
+                                <option value="Feb">Feb</option>
+                                <option value="Mar">Mar</option>
+                                <option value="Apr">Apr</option>
+                                <option value="May">May</option>
+                                <option value="Jun">Jun</option>
+                                <option value="Jul">Jul</option>
+                                <option value="Aug">Aug</option>
+                                <option value="Sep">Sep</option>
+                                <option value="Oct">Oct</option>
+                                <option value="Nov">Nov</option>
+                                <option value="Dec">Dec</option>
+                            </select>
+
+                            <select name="end_year" class="form-control form-control-sm my-select">
                                 <option value="">--Select Year</option>
                             <?php for ($i = 2000; $i <= date('Y'); $i++) { ?>
                                 <option value="<?= $i ?>"><?= $i ?></option>
@@ -65,12 +94,12 @@ $employee_model = new Employee_model();
                     </div>
 
                     <button type="submit" class="btn btn-sm btn-primary" id="submitBtn">Save</button>
-
+                </div>
                 </form>
             </div>
         </div>
     </div>
-    <div class="col-lg-8">
+    <div class="col-lg-12">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0"><?= $title ?> List</h4>
