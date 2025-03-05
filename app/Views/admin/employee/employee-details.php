b68fb94efa1f993796ff36389c8de8d1923dddc0
Mithun021 <mithunkr79038@gmail.com>
2025-03-05 17:36:11 +0530

    Adjunt Faculty

--- app/Views/admin/employee/employee-details.php
+++ app/Views/admin/employee/employee-details.php
@@ -129,7 +129,7 @@
                                 <div class="text-center">
                                     <img src="https://via.placeholder.com/150" alt="image" class="rounded-circle avatar-lg mb-3">
                                     <?php if (!empty($employee_details['profile_photo']) && file_exists('public/admin/uploads/employee/' . $employee_details['profile_photo'])): ?>
-                                        <a href="<?= base_url() ?>public/admin/uploads/employee/<?= $employee_details['profile_photo'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/uploads/employee/<?= $employee_details['profile_photo'] ?>" alt="" class="d-flex align-self-center rounded mr-3 border" height="80px"></a>
+                                        <img src="<?= base_url() ?>public/admin/uploads/employee/<?= $employee_details['profile_photo'] ?>" alt="" class="d-flex align-self-center avatar-lg rounded mr-3 border" height="80px">
                                     <?php else: ?>
                                         <img src="<?= base_url() ?>public/admin/uploads/employee/invalid_image.png" alt="" height="40px">
                                     <?php endif; ?>
