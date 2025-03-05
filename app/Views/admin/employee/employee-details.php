3de899bc68d6a8e02d1e5078f269aeb7597fdd18
Mithun021 <mithunkr79038@gmail.com>
2025-03-05 17:42:30 +0530

    Adjunt Faculty

--- app/Views/admin/employee/employee-details.php
+++ app/Views/admin/employee/employee-details.php
@@ -134,7 +134,7 @@
                                     <?php endif; ?>
                                     <h4 class="mb-0"><?= $employee_details['sir_name'] . " " .$employee_details['first_name'] . " " . $employee_details['middle_name'] . " " . $employee_details['last_name'] ?></h4>
                                     <p class="text-muted"><?= $employee_details['employee_unique_id'] ?></p>
-                                    <button type="button" class="btn btn-outline-success btn-rounded waves-effect waves-light"><i class="far fa-download"></i> Download Resume</button>
+                                    <button type="button" class="btn btn-outline-success btn-rounded waves-effect waves-light"><i class="mdi mdi-cloud-download-outline"></i> Download Resume</button>
                                 </div>
                             </div>
                         </div>
