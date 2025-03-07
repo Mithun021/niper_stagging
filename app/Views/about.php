34c4ce34686ad2a65fadb25940d826977e30178f
Mithun021 <mithunkr79038@gmail.com>
2025-03-07 16:57:38 +0530

    Your commit message

--- app/Views/about.php
+++ app/Views/about.php
@@ -1,6 +1,6 @@
 <?= $this->extend("layouts/master") ?>
 <?=  $this->section("body-content"); ?>
 
-<h1>About Us page New Update</h1>
+<h1>About Us page New Update 2</h1>
 
 <?= $this->endSection() ?>
