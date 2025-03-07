ffd22207d65eb29beffe542d992f56b170192e9f
Mithun021 <mithunkr79038@gmail.com>
2025-03-07 17:54:39 +0530

    Your commit message

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://niper.brandsbanao.co.in/';
+    public string $baseURL = 'https://niper.dcodematerials.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
