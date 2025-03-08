3bc0ed1f1d3b0395f57d8451463907d64df80203
Mithun021 <mithunkr79038@gmail.com>
2025-03-08 12:44:00 +0530

    Your commit message

--- app/Views/admin/facility/facility-notification.php
+++ app/Views/admin/facility/facility-notification.php
@@ -99,7 +99,7 @@
                             <td><?= $key+1 ?></td>
                             <td>
                                 <?php if (!empty($value['upload_file']) && file_exists('public/admin/uploads/facilities/' . $value['upload_file'])): ?>
-                                    <a href="<?= base_url() ?>public/admin/uploads/facilities/<?= $value['upload_file'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/uploads/facilities/<?= $value['upload_file'] ?>" alt="" height="30px"></a>
+                                    <a href="<?= base_url() ?>public/admin/uploads/facilities/<?= $value['upload_file'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/assets/images/pdf.png" alt="" height="30px"></a>
                                 <?php else: ?>
                                     <img src="<?= base_url() ?>public/admin/uploads/facilities/invalid_image.png" alt="" height="40px">
                                 <?php endif; ?>
