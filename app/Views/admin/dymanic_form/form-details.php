7a3550b22575937969289bea573e65a14ab8a150
Mithun021 <mithunkr79038@gmail.com>
2025-03-08 17:09:52 +0530

    Your commit message

--- app/Views/admin/dymanic_form/form-details.php
+++ app/Views/admin/dymanic_form/form-details.php
@@ -21,28 +21,28 @@
                         echo session()->getFlashdata('msg');
                     }
                 ?>
-                <form method="post" action="<?= base_url() ?>admin/designation">
+                <form method="post" action="<?= base_url() ?>admin/form-details" enctype="multipart/form-data">
                     <div class="form-group">
-                        <span for="">Designation Name<span class="text-danger">*</span></span>
-                        <input type="text" class="form-control form-control-sm" name="designation_title" required minlength="3">
+                        <span for="">Form Name<span class="text-danger">*</span></span>
+                        <input type="text" class="form-control form-control-sm" name="name" required minlength="3">
                     </div>
-                    <!-- <div class="form-group">
+                    <div class="form-group">
                         <span for="">Desription</span>
                         <textarea id="editor" name="description"></textarea>
-                    </div> -->
+                    </div>
                     <div class="form-group">
-                        <span for="">Designation Holds</span>
-                        <select name="designation_hold" class="form-control form-control-sm">
-                            <option value="Teaching">Teaching</option>
-                            <option value="Non-teaching">Non-teaching</option>
-                            <option value="Administrative">Administrative</option>
-                        </select>
+                        <span for="">Form Banner Image(Png, Jpeg)<span class="text-danger">*</span></span>
+                        <input type="file" class="form-control form-control-sm" name="upload_file" required accept=".jpg,.jpeg,.png">
+                    </div>
+                    <div class="form-group">
+                        <span for="">Publish Date<span class="text-danger">*</span></span>
+                        <input type="date" class="form-control form-control-sm" name="publish_date" required>
                     </div>
                     <div class="form-group">
                         <span>Status</span>
                         <select name="status" id="status" class="form-control form-control-sm">
-                            <option value="1">Active</option>
-                            <option value="0">Inactive</option>
+                            <option value="1">Accepting</option>
+                            <option value="0">Not Accepting</option>
                         </select>
                     </div>
 
@@ -57,17 +57,18 @@
             <div class="card-header">
                 <h4 class="card-title m-0"><?= $title ?> List</h4>
             </div>
-            <div class="card-body">
+            <div class="card-body p-2">
                 <div class="table-responsive">
                 <table class="table table-striped table-hover" id="basic-datatable">
                     <thead>
                         <tr>
                             <td>SN</td>
+                            <td>Banner</td>
                             <td>Title</td>
-                            <td>Desig. Hold</td>
+                            <td>Description</td>
+                            <td>Publish Date</td>
                             <td>Status</td>
                             <td>Upload by</td>
-                            <td>Create at</td>
                             <td>Action</td>
                         </tr>
                     </thead>
