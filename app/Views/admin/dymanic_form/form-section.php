dd760d632d7ab817a43856c4e594542ac737257c
Mithun021 <mithunkr79038@gmail.com>
2025-03-08 17:41:44 +0530

    Your commit message

--- app/Views/admin/dymanic_form/form-section.php
+++ app/Views/admin/dymanic_form/form-section.php
@@ -13,7 +13,7 @@ use App\Models\Form_details_model;
 </style>
 <!-- start page title --> 
 <div class="row">
-    <div class="col-lg-4">
+    <div class="col-lg-3">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Add <?= $title ?> </h4>
@@ -49,12 +49,12 @@ use App\Models\Form_details_model;
             </div>
         </div>
     </div>
-    <div class="col-lg-8">
+    <div class="col-lg-9">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0"><?= $title ?> List</h4>
             </div>
-            <div class="card-body">
+            <div class="card-body p-2">
                 <div class="table-responsive">
                 <table class="table table-striped table-hover" id="basic-datatable">
                     <thead>
