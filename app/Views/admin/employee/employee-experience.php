bc870625c37e82ed3d64c13541d135bd3882b926
Mithun021 <mithunkr79038@gmail.com>
2025-03-17 12:43:45 +0530

    Your commit message

--- app/Views/admin/employee/employee-experience.php
+++ app/Views/admin/employee/employee-experience.php
@@ -84,6 +84,7 @@ $employee_model = new Employee_model();
                                     </select>
 
                                     <div id="work_details" style="display: none;">
+                                        <span>Description of Works</span>
                                         <input type="text" name="work_description" id="work_description" class="form-control form-control-sm">
                                     </div>
                                 </div>
