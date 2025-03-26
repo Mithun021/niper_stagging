e4b4ba88b0a9dff04988b08a830d4eb96829782b
Mithun021 <mithunkr79038@gmail.com>
2025-03-26 12:11:38 +0530

    Your commit message

--- app/Views/admin/dymanic_form/manage-questions.php
+++ app/Views/admin/dymanic_form/manage-questions.php
@@ -12,6 +12,9 @@
 <div class="row">
     <div class="col-lg-12">
         <div class="card">
+            <div class="card-header">
+                <h4 class="card-title m-0"><?= $title ?> </h4>
+            </div>
             <div class="card-body">
                 <?php
                     if(session()->getFlashdata('status')){
