04a59f356cfe966012539d50c8a715dee3b313e7
Mithun021 <mithunkr79038@gmail.com>
2025-03-26 17:59:28 +0530

    Your commit message

---
+++ app/Models/Manage_question_model.php
@@ -0,0 +1,30 @@
+<?php
+    namespace App\Models;
+    use CodeIgniter\Model;
+    class Manage_question_model extends Model
+    {
+        protected $table         = 'manage_question';
+        protected $primaryKey    = 'id';
+        protected $allowedFields = ['form_section_id','question_type','question_details_id'];
+        protected $createdField  = 'created_at';
+
+        public function add($data, $id = null) {
+            if ($id != null) {
+                $result = $this->update($id, $data);
+                return $result ? true : 'Data not updated: Update failed.';
+            } else {
+                $result = $this->insert($data);
+                return $result ? true : 'Data not inserted: Insertion failed.';
+            }
+        }
+
+        public function get($id = null){
+            if($id != null){
+                $result = $this->where('id',$id)->first();
+            }else{
+                $result = $this->orderBy('id','asc')->findAll();
+            }
+            return $result;
+        }
+    }
+?>
