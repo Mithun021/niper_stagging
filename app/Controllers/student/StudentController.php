c13f991f21771f6d7cce4eaa4eea00b5f368cb7d
Mithun021 <mithunkr79038@gmail.com>
2025-04-15 18:04:06 +0530

    first commit

--- app/Controllers/student/StudentController.php
+++ app/Controllers/student/StudentController.php
@@ -8,6 +8,7 @@ class StudentController extends BaseController
 {
     public function index()
     {
+        $data['title'] = 'Student List';
         return view('students/index'); // points to app/Views/student/index.php
     }
 }
