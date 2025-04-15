12c35a65a4a6f1972c8e97d2f1ba6b7921faa605
Mithun021 <mithunkr79038@gmail.com>
2025-04-15 18:05:04 +0530

    first commit

--- app/Controllers/student/StudentController.php
+++ app/Controllers/student/StudentController.php
@@ -8,7 +8,7 @@ class StudentController extends BaseController
 {
     public function index()
     {
-        $data['title'] = 'Student List';
-        return view('students/index'); // points to app/Views/student/index.php
+        $data = ['title' =>'Student Dashboard'];
+        return view('students/index',$data); 
     }
 }
