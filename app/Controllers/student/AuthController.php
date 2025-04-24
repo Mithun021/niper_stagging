7c502a39fc81408f0d885f4e8d17e003005391d3
Mithun021 <mithunkr79038@gmail.com>
2025-04-24 14:09:15 +0530

    student dashboard

--- app/Controllers/student/AuthController.php
+++ app/Controllers/student/AuthController.php
@@ -3,17 +3,36 @@
 namespace App\Controllers\student;
 
 use App\Controllers\BaseController;
+use App\Models\Student_model;
 
 class AuthController extends BaseController
 {
     public function login()
     {
-        $data = ['title' =>'Student Login'];
+        $student_model = new Student_model();
+        $data = ['title' => 'Student Login'];
         if ($this->request->is('get')) {
-            return view('student/login',$data);
-        }else if ($this->request->is('post')) {
-            echo "ok";
+            return view('student/login', $data);
+        } else if ($this->request->is('post')) {
+            $userId = $this->request->getPost('userId');
+            $userPassword = $this->request->getVar('userPassword');
+
+            $data = $student_model->where('enrollment_no', $userId)->first();
+            if ($data) {
+                $session_data = [
+                    'loggedstudentName' => $data['first_name'],
+                    'loggedstudentId' => $data['id']
+                ];
+                if (password_verify($userPassword, $data['password'])) {
+                    $this->session->set('loggedStudentData', $session_data);
+                    $this->session->set('studentLoginned', "studentLoginned");
+                    echo "dataMatch";
+                } else {
+                    echo 'User ID or Password Mismatch';
+                }
+            } else {
+                echo "Given Userid not found";
+            }
         }
-         
     }
 }
