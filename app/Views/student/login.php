238dc47623a767d8ded95025cd082032cc55baeb
Mithun021 <mithunkr79038@gmail.com>
2025-04-24 13:39:38 +0530

    student dashboard

--- app/Views/student/login.php
+++ app/Views/student/login.php
@@ -30,10 +30,12 @@
  
     <div>
         <div class="container">
-                    <div class="d-flex justify-content-center align-items-center min-vh-100">
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-flex align-items-center min-vh-100">
                         <div class="d-block bg-white shadow-lg rounded my-5">
                             <div class="row">
-                                <div class="col-lg-6">
+                                <div class="col-lg-12">
                                     <div class="p-5">
                                         <div class="text-center mb-5">
                                             <a href="<?= base_url() ?>" class="text-dark font-size-22 font-family-secondary">
@@ -41,7 +43,7 @@
                                             </a>
                                         </div>
                                         <h1 class="h5 mb-1">Student Login</h1>
-                                        <p class="text-muted mb-4">Enter your University and password.</p>
+                                        <p class="text-muted mb-4">Enter your email address and password to access admin panel.</p>
                                         <form class="user"  id="adminLoginForm">
                                             <div class="form-group">
                                                 <input type="text" name="username" class="form-control form-control-user" id="username" placeholder="Phone/Email">
@@ -63,6 +65,8 @@
                             </div> <!-- end row -->
                         </div> <!-- end .w-100 -->
                     </div> <!-- end .d-flex -->
+                </div> <!-- end col-->
+            </div> <!-- end row -->
         </div>
         <!-- end container -->
     </div>
