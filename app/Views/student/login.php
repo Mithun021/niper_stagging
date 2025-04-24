ad42fc917645f965845e24b0f46dbabf50b20aff
Mithun021 <mithunkr79038@gmail.com>
2025-04-24 13:31:36 +0530

    student dashboard

--- app/Views/student/login.php
+++ app/Views/student/login.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,7 +18,7 @@
     <link href="<?= base_url() ?>public/admin/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="<?= base_url() ?>public/admin/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
     <style>
-        .error {
+        .error{
             color: red;
             font-weight: normal;
         }
@@ -26,37 +27,44 @@
 </head>
 
 <body style="background-image: url('<?= base_url() ?>public/admin/assets/images/niperclg.jpg'); background-size: cover;">
-
+ 
     <div>
         <div class="container">
-            <div class="row d-flex justify-content-center align-items-center min-vh-100">
-                <div class="col-lg-6">
-                    <div class="p-5">
-                        <div class="text-center mb-5">
-                            <a href="<?= base_url() ?>" class="text-dark font-size-22 font-family-secondary">
-                                <img src="<?= base_url() ?>public/assets/image/logo.jpg" alt="" height="90">
-                            </a>
-                        </div>
-                        <h1 class="h5 mb-1">Student Login</h1>
-                        <p class="text-muted mb-4">Enter your University roll no and password.</p>
-                        <form class="user" id="adminLoginForm">
-                            <div class="form-group">
-                                <input type="text" name="username" class="form-control form-control-user" id="username" placeholder="Phone/Email">
-                            </div>
-                            <div class="form-group">
-                                <input type="password" name="password" class="form-control form-control-user" id="userpassword" placeholder="Password">
-                            </div>
-                            <button type="submit" class="btn btn-success btn-block waves-effect waves-light"> Log In </button>
-                        </form>
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-flex align-items-center min-vh-100">
+                        <div class="d-block bg-white shadow-lg rounded my-5">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="p-5">
+                                        <div class="text-center mb-5">
+                                            <a href="<?= base_url() ?>" class="text-dark font-size-22 font-family-secondary">
+                                            <img src="<?= base_url() ?>public/assets/image/logo.jpg" alt="" height="90">
+                                            </a>
+                                        </div>
+                                        <h1 class="h5 mb-1">Student Login</h1>
+                                        <p class="text-muted mb-4">Enter your University and password.</p>
+                                        <form class="user"  id="adminLoginForm">
+                                            <div class="form-group">
+                                                <input type="text" name="username" class="form-control form-control-user" id="username" placeholder="Phone/Email">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" name="password" class="form-control form-control-user" id="userpassword" placeholder="Password">
+                                            </div>
+                                            <button type="submit" class="btn btn-success btn-block waves-effect waves-light"> Log In </button>
+                                        </form>
 
-                        <div class="row mt-4">
-                            <!-- <div class="col-12 text-center">
+                                        <div class="row mt-4">
+                                            <!-- <div class="col-12 text-center">
                                                 <p class="text-muted mb-2"><a href="pages-recoverpw.html" class="text-muted font-weight-medium ml-1">Forgot your password?</a></p>
                                             </div> end col -->
-                        </div>
-                        <!-- end row -->
-                    </div> <!-- end .padding-5 -->
-
+                                        </div>
+                                        <!-- end row -->
+                                    </div> <!-- end .padding-5 -->
+                                </div> <!-- end col -->
+                            </div> <!-- end row -->
+                        </div> <!-- end .w-100 -->
+                    </div> <!-- end .d-flex -->
                 </div> <!-- end col-->
             </div> <!-- end row -->
         </div>
@@ -79,51 +87,51 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.20.0/jquery.validate.min.js"></script>
 
     <script>
-        $(document).ready(function() {
+        $(document).ready(function(){
             $("#adminLoginForm").validate({
-                rules: {
-                    username: {
+                rules:{
+                    username : {
                         required: true,
                         minlength: 3
                     },
-                    password: {
+                    password : {
                         required: true,
                         minlength: 6 // Minimum length for password
                     }
-                },
-                messages: {
-                    username: {
+                },messages: {
+                    username : {
                         required: "Required User ID",
                         minlength: "Your ID must be at least 3 characters long"
                     },
-                    password: {
+                    password : {
                         required: "Please enter a password",
                         minlength: "Password must be at least 6 characters"
                     }
                 },
-                submitHandler: function(form) {
+                submitHandler: function (form) {
                     var requestedData = {
-                        userId: $('#username').val(),
-                        userPassword: $('#userpassword').val()
+                        userId : $('#username').val(),
+                        userPassword : $('#userpassword').val()
                     }
                     // console.log(requestedData);
                     $.ajax({
                         type: "post",
                         url: "<?= base_url() ?>student/login",
                         data: requestedData,
-                        success: function(response) {
-                            if (response == "dataMatch") {
+                        success: function (response) {
+                            if(response == "dataMatch") {
                                 Swal.fire({
                                     position: "top-end",
                                     icon: "success",
                                     title: "Account Successfully Login",
                                     showConfirmButton: false,
                                     timer: 2500
-                                }).then(function() {
+                                }).then(function(){
                                     window.location.href = "<?= base_url() ?>admin/";
                                 });
-
-                            } else {
+                                
+                            }  
+                            else{
                                 Swal.fire({
                                     position: "top-end",
                                     icon: "error",
@@ -136,8 +144,8 @@
                     });
                 }
             });
-        });
-    </script>
+    });
+</script>
 
 </body>
 
