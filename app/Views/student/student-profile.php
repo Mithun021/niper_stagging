3f1b7bba138e1580bc10f5037672aedaa6769e99
Mithun021 <mithunkr79038@gmail.com>
2025-04-24 16:52:00 +0530

    student dashboard

--- app/Views/student/student-profile.php
+++ app/Views/student/student-profile.php
@@ -5,7 +5,7 @@
 <div class="row">
     <div class="col-lg-12">
         <div class="card card-body">
-            <h1>Welcome to NIPER Student Portal</h1>
+            
         </div>
     </div>
 </div>
