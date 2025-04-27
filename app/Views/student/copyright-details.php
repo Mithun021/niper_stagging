1c3f6537c334cde8bed1587f4d42ea5c6df001da
Mithun021 <mithunkr79038@gmail.com>
2025-04-27 16:08:56 +0530

    student dashboard

--- app/Views/student/copyright-details.php
+++ app/Views/student/copyright-details.php
@@ -4,8 +4,107 @@
 <!-- start page title -->
 <div class="row">
     <div class="col-lg-12">
-        <div class="card card-body">
-            <h1>Welcome to NIPER Student Portal</h1>
+        <div class="card">
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <h4 class="card-title m-0"><?= $title ?></h4>
+            </div>
+            <form action="<?= base_url() ?>student/patent-details" method="post" enctype="multipart/form-data">
+                <div class="card-body">
+                    <?php if (session()->getFlashdata('status')): ?>
+                        <?= session()->getFlashdata('status') ?>
+                    <?php endif; ?>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <span for="Pubtitle">Patent Title:<span class="text-danger">*</span></span>
+                                <input name="patent_title" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <span for="Pubdesc">Description:<span class="text-danger">*</span></span>
+                                <textarea id="editor" name="description"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="addServicetable">
+                                    <thead class="bg-light">
+                                        <tr>
+                                            <td scope="col">Author Details<span class="text-danger">*</span></td>
+                                            <td scope="col"><button type="button" class="btn btn-sm btn-primary" id="addnewservicerow">+</button></td>
+                                        </tr>
+
+                                    </thead>
+                                    <tbody id="stockTbody">
+                                        <tr id="stockTrow">
+                                            <td>
+                                                <input type="text" class="form-control" name="author_name[]" placeholder="Enter Author Name" required>
+                                            </td>
+                                            <td>
+                                                <button type="button" class="btn btn-sm btn-danger" id="removenewServicerow">-</button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="DoIdetails">Patent Number<span class="text-danger">*</span></span>
+                                <input type="text" name="patent_number" id="patent_number" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Pubtype">Patent Status:<span class="text-danger">*</span></span>
+                                <select name="patent_status" id="patent_status" class="form-control form-control-sm" required>
+                                    <option value="">--Select--</option>
+                                    <option value="Submitted">Submitted</option>
+                                    <option value="Granted">Granted</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="DoIdetails">Patent Filing Date <span class="text-danger">*</span></span>
+                                <input type="date" name="patent_filing_date" id="patent_filing_date" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-6" id="patent_grant_date" style="display: none;">
+                            <div class="form-group">
+                                <span for="DoIdetails">Patent Grant Date<span class="text-danger">*</span></span>
+                                <input type="date" name="patent_grant_date" id="patent_grant_date" class="form-control form-control-sm">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Pubtype">Patent Level :<span class="text-danger">*</span></span>
+                                <select name="patent_level" id="patent_level" class="form-control form-control-sm" required>
+                                    <option value="">--Select--</option>
+                                    <option value="National">National</option>
+                                    <option value="International">International</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="DoIdetails">Fund Generated<span class="text-danger">*</span></span>
+                                <input type="text" name="fund_generated" id="patent_number" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="form-group">
+                                <span>File Upload Option(.pdf)<span class="text-danger">*</span></span>
+                                <input type="file" class="form-control form-control-sm" name="file_upload" accept=".pdf" required>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer py-1">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
