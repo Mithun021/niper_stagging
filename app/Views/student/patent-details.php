3a38713ff6de46edd87afa9aabc8c948252f9e90
Mithun021 <mithunkr79038@gmail.com>
2025-04-27 14:43:39 +0530

    student dashboard

--- app/Views/student/patent-details.php
+++ app/Views/student/patent-details.php
@@ -1,11 +1,63 @@
 <?= $this->extend("student/stdlayouts/master") ?>
-<?=  $this->section("student-content"); ?>
+<?= $this->section("student-content"); ?>
 
 <!-- start page title -->
 <div class="row">
     <div class="col-lg-12">
-        <div class="card card-body">
-            <h1>Welcome to NIPER Student Portal</h1>
+        <div class="card">
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <h4 class="card-title m-0"><?= $title ?></h4>
+            </div>
+            <form action="<?= base_url() ?>student/patent-details" method="post" enctype="multipart/form-data">
+                <div class="card-body">
+                    <?php if (session()->getFlashdata('status')): ?>
+                        <?= session()->getFlashdata('status') ?>
+                    <?php endif; ?>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <span for="Pubtitle">Chapter Title:<span class="text-danger">*</span></span>
+                                <input name="chapter_title" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <span for="Pubtitle">Book Title:<span class="text-danger">*</span></span>
+                                <input name="book_title" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <span for="Pubdesc">Publication Description:<span class="text-danger">*</span></span>
+                                <textarea id="editor" name="publication_description"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="addServicetable">
+                                    <thead class="bg-light">
+                                        <tr>
+                                            <td scope="col">Author Details<span class="text-danger">*</span></td>
+                                            <td scope="col"><button type="button" class="btn btn-sm btn-primary" id="addnewservicerow">+</button></td>
+                                        </tr>
+
+                                    </thead>
+                                    <tbody id="stockTbody">
+                                        <tr id="stockTrow">
+                                            <td>
+                                                <input type="text" class="form-control" name="author_name[]" placeholder="Enter Author Name" required>
+                                            </td>
+                                            <td>
+                                                <button type="button" class="btn btn-sm btn-danger" id="removenewServicerow">-</button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
