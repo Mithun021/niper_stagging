47d9d70758ca71579c9791bdb27033c616c70c4d
Mithun021 <mithunkr79038@gmail.com>
2025-04-27 14:13:46 +0530

    student dashboard

--- app/Views/student/book-chapter-details.php
+++ app/Views/student/book-chapter-details.php
@@ -170,7 +170,7 @@ $student_bookchapter_author_model = new Student_bookchapter_author_model();
                                     <tr id="stockTrow">
                                         <td><?= $pub['chapter_title'] ?></td>
                                         <td><?= $pub['book_title'] ?></td>
-                                        <td>
+                                        <td style="width: 90px;">
                                             <?php $authors = $student_bookchapter_author_model->getByBookchapter($pub['id']); ?>
                                             <?php if ($authors): ?>
                                                 <?php foreach ($authors as $author): ?>
