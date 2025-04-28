9ddba1c6e474f02310f6a60d720ea20f410ce68b
Mithun021 <mithunkr79038@gmail.com>
2025-04-28 15:55:32 +0530

    student dashboard

--- app/Views/student/student-profile.php
+++ app/Views/student/student-profile.php
@@ -4,8 +4,21 @@
 <!-- start page title -->
 <div class="row">
     <div class="col-lg-12">
-        <div class="card card-body">
-            
+        <div class="card card-body p-1">
+            <div class="resume-header">
+                <div class="student-image">
+                    <img class="header-profile-user" src="<?= base_url() ?>public/assets/image/avatar.png"
+                alt="Header Avatar">
+                </div>
+                <div class="student-personal-details">
+                    <h4>MITHUN KUMAR</h4>
+                    <p>Email :</p>
+                    <p>Phone :</p>
+                    <p>Father's Name :</p>
+                    <p>Address :</p>
+                    <p>LinkedIn :</p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
