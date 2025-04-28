d4d17b94ec05eaf982542eaf4aae4e48747b58cc
Mithun021 <mithunkr79038@gmail.com>
2025-04-28 11:33:23 +0530

    student dashboard

--- app/Views/student/experience-details.php
+++ app/Views/student/experience-details.php
@@ -14,13 +14,53 @@
                         <?= session()->getFlashdata('status') ?>
                     <?php endif; ?>
                     <div class="row">
-                        <div class="col-lg-12">
+                        <div class="col-lg-6">
                             <div class="form-group">
-                                <span for="Pubtitle">Achievement Title:<span class="text-danger">*</span></span>
-                                <input name="achievement_title" class="form-control form-control-sm" required>
+                                <span for="Pubtitle">Designation:<span class="text-danger">*</span></span>
+                                <input name="designation" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Pubtitle">Name of Organization:<span class="text-danger">*</span></span>
+                                <input name="organization_name" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Pubtitle">Organization Type :<span class="text-danger">*</span></span>
+                                <select name="organization_type" class="form-control form-control-sm" required>
+                                    <option value="">--Select--</option>
+                                    <option value="Central Government">Central Government</option>
+                                    <option value="State Government">State Government</option>
+                                    <option value="Autonomous">Autonomous</option>
+                                    <option value="Private">Private</option>
+                                    <option value="PSU">PSU</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Pubtitle">Date of Joining:<span class="text-danger">*</span></span>
+                                <input name="joining_date" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Pubtitle">Date of Relieving:</span>
+                                <input name="releiving_date" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="form-group">
+                                <span>File Upload Option(.pdf)<span class="text-danger">*</span></span>
+                                <input type="file" class="form-control form-control-sm" name="file_upload" accept=".pdf" required>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="card-footer py-1">
+                    <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
             </form>
         </div>
