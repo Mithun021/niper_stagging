58b9c06d79d9a7d5a6913c7652d8677aaa786f01
Mithun021 <mithunkr79038@gmail.com>
2025-04-28 09:14:21 +0530

    student dashboard

--- app/Views/student/achievement-details.php
+++ app/Views/student/achievement-details.php
@@ -4,8 +4,59 @@
 <!-- start page title -->
 <div class="row">
     <div class="col-lg-12">
-        <div class="card card-body">
-            <h1>Welcome to NIPER Student Portal</h1>
+        <div class="card">
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <h4 class="card-title m-0"><?= $title ?></h4>
+            </div>
+            <form action="<?= base_url() ?>student/achievement-details" method="post" enctype="multipart/form-data">
+                <div class="card-body">
+                    <?php if (session()->getFlashdata('status')): ?>
+                        <?= session()->getFlashdata('status') ?>
+                    <?php endif; ?>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <span for="Pubtitle">Achievement Title:<span class="text-danger">*</span></span>
+                                <input name="achievement_title" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <span for="Pubdesc">Description:<span class="text-danger">*</span></span>
+                                <textarea id="editor" name="description"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="DoIdetails">Awarded Agency<span class="text-danger">*</span></span>
+                                <input type="text" name="awarded_agency" id="awarded_agency" class="form-control form-control-sm" required>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="Pubtype">Award Level:<span class="text-danger">*</span></span>
+                                <select name="award_level" id="award_level" class="form-control form-control-sm" required>
+                                    <option value="">--Select--</option>
+                                    <option value="National">National</option>
+                                    <option value="International">International</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <span for="DoIdetails">Award Date<span class="text-danger">*</span></span>
+                                <input type="date" name="award_date" id="award_date" class="form-control form-control-sm">
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="form-group">
+                                <span>File Upload Option(.pdf)<span class="text-danger">*</span></span>
+                                <input type="file" class="form-control form-control-sm" name="file_upload" accept=".pdf" required>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
