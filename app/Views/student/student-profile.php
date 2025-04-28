6135b77708a33f442059d716f325b871d23e3913
Mithun021 <mithunkr79038@gmail.com>
2025-04-28 15:57:57 +0530

    student dashboard

--- app/Views/student/student-profile.php
+++ app/Views/student/student-profile.php
@@ -2,16 +2,70 @@
 <?=  $this->section("student-content"); ?>
 
 <style>
+    p,h3,h4,h5,h6{
+        margin: 0;
+        padding: 0;
+    }
     .flex-div{
         display: flex;
         justify-content: flex-start;
+        /* align-items: center; */
+    }
+    .justify-div{
+        display: flex;
+        justify-content: space-between;
         align-items: center;
+    }<?= $this->extend("student/stdlayouts/master") ?>
+<?=  $this->section("student-content"); ?>
+
+<style>
+    p,h3,h4,h5,h6{
+        margin: 0;
+        padding: 0;
+    }
+    .flex-div{
+        display: flex;
+        justify-content: flex-start;
+        /* align-items: center; */
     }
     .justify-div{
         display: flex;
         justify-content: space-between;
         align-items: center;
     }
+</style>
+
+<!-- start page title -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card card-body p-1">
+            <div class="resume-header flex-div">
+                <div class="student-image">
+                    <img class="header-profile-user" src="<?= base_url() ?>public/assets/image/avatar.png"
+                alt="Header Avatar">
+                </div>
+                <div class="student-personal-details">
+                    <h4>MITHUN KUMAR</h4>
+                    <p>Email :</p>
+                    <p>Phone :</p>
+                    <p>Father's Name :</p>
+                    <p>Address :</p>
+                    <p>LinkedIn :</p>
+                </div>
+            </div>
+            <div class="logs">
+                <h4>Logs</h4>
+                <ul>
+                    <li>Login History</li>
+                    <li>Activity Logs</li>
+                    <li>System Logs</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection() ?>
 </style>
 
 <!-- start page title -->
