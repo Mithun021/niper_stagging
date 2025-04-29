61c42b0a3abd0e92909dbdac193e9d7234add2e5
Mithun021 <mithunkr79038@gmail.com>
2025-04-29 18:17:23 +0530

    student dashboard

--- app/Views/admin/placement/company-details.php
+++ app/Views/admin/placement/company-details.php
@@ -9,7 +9,7 @@
 </style>
 <!-- start page title -->
 <div class="row">
-    <div class="col-lg-12">
+    <div class="col-md-12">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Add <?= $title ?></h4>
