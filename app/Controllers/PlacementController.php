62b81cdf9744b2d0ad23ed49fe845ab13b956856
Mithun021 <mithunkr79038@gmail.com>
2025-04-29 18:22:53 +0530

    student dashboard

--- app/Controllers/PlacementController.php
+++ app/Controllers/PlacementController.php
@@ -104,25 +104,5 @@ class PlacementController extends BaseController
         }
     }
 
-    // public function company_details()
-    // {
-    //     $data = ['title' => 'Company Details'];
-    //     if($this->request->is('get')) {
-    //         return view('admin/placement/company-details',$data);
-    //     } else if($this->request->is('post')){
-            
-    //     }
-    // }
-
-    // public function company_details()
-    // {
-    //     $data = ['title' => 'Company Details'];
-    //     if($this->request->is('get')) {
-    //         return view('admin/placement/company-details',$data);
-    //     } else if($this->request->is('post')){
-            
-    //     }
-    // }
-
 
 }
