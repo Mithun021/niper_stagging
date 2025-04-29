a440fe766a31a1c20fe476630c6f6fb5383988f5
Mithun021 <mithunkr79038@gmail.com>
2025-04-29 18:11:03 +0530

    student dashboard

--- app/Controllers/PlacementController.php
+++ app/Controllers/PlacementController.php
@@ -4,5 +4,20 @@ namespace App\Controllers;
 
 class PlacementController extends BaseController
 {
-    
+    public function company_details()
+    {
+        $data = ['title' => 'Company Details'];
+        if($this->request->is('get')) {
+            return view('admin/placement/company-details',$data);
+        } else if($this->request->is('post')){
+            
+        }
+    }
+
+
+
+    public function placementList()
+    {
+        return view('placement/placementList');
+    }
 }
