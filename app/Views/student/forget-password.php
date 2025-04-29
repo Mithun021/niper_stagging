0a5bb58489aeae93c932364561b7dc3f4c21cd49
Mithun021 <mithunkr79038@gmail.com>
2025-04-29 14:43:33 +0530

    student dashboard

--- app/Views/student/forget-password.php
+++ app/Views/student/forget-password.php
@@ -31,17 +31,12 @@
 <div>
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-6">
+                <div class="col-5">
                     <div class="d-flex align-items-center min-vh-100">
                         <div class="w-100 d-block bg-white shadow-lg rounded my-5">
                             <div class="row">
                                 <div class="col-lg-12">
                                     <div class="p-5">
-                                        <div class="text-center mb-5">
-                                            <a href="index.html" class="text-dark font-size-22 font-family-secondary">
-                                                <i class="mdi mdi-alpha-x-circle"></i> <b>XELORO</b>
-                                            </a>
-                                        </div>
                                         <h1 class="h5 mb-1">Reset Password</h1>
                                         <p class="text-muted mb-4">Enter your email address and we'll send you an email with instructions to reset your password.</p>
                                         <form>
@@ -55,8 +50,7 @@
 
                                         <div class="row mt-5">
                                             <div class="col-12 text-center">
-                                                <p class="text-muted">Already have account?  <a href="pages-login.html" class="text-muted font-weight-medium ml-1"><b>Sign In</b></a></p>
-                                                <p class="text-muted mb-0">Don't have an account? <a href="pages-register.html" class="text-muted font-weight-medium ml-1"><b>Sign Up</b></a></p>
+                                                <p class="text-muted">Already have account?  <a href="<?= base_url() ?>stdlogin" class="text-muted font-weight-medium ml-1"><b>Sign In</b></a></p>
                                             </div> <!-- end col -->
                                         </div>
                                         <!-- end row -->
