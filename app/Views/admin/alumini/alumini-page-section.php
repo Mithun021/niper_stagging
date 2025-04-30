b5f1e45946e00a5a744f7f5df3580cbee862f17f
Mithun021 <mithunkr79038@gmail.com>
2025-04-30 11:59:52 +0530

    student dashboard

--- app/Views/admin/alumini/alumini-page-section.php
+++ app/Views/admin/alumini/alumini-page-section.php
@@ -9,18 +9,37 @@
 </style>
 <!-- start page title -->
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-4">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Add <?= $title ?></h4>
             </div>
-            <div class="card-body">
-                <?php
-                    if(session()->getFlashdata('status')){
+            <form action="<?= base_url() ?>admin/alumini-page-section" method="post" enctype="multipart/form-data" id="alumini-page-notification-form">
+                <div class="card-body p-1">
+                    <?php if (session()->getFlashdata('status')) {
                         echo session()->getFlashdata('status');
-                    }
-                ?>
-            </div>
+                    } ?>
+                    <div class="form-group">
+                        <span for="title">Section Title</span>
+                        <input type="text" class="form-control form-control-sm" name="title" id="title" placeholder="Enter Title" required>
+                    </div>
+                    <div class="form-group">
+                        <span for="title">Description</span>
+                        <textarea name="description" id="editor"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <span for="title">Section priority</span>
+                        <input type="number" name="priority" class="form-control form-control-sm">
+                    </div>
+                    <div class="form-group">
+                        <span for="title">Section image (Multiple image option )</span>
+                        <input type="file" class="form-control form-control-sm" name="file_upload" accept=".jpg,.png,.jpeg" required>
+                    </div>
+                </div>
+                <div class="card-footer p-2">
+                    <input type="submit" class="btn btn-primary" value="Submit" id="submit">
+                </div>
+            </form>
         </div>
     </div>
 </div>
