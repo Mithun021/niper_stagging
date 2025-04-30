a51893b5fc4636b20e8dd676d60373f8c8d3a607
Mithun021 <mithunkr79038@gmail.com>
2025-04-30 11:13:20 +0530

    student dashboard

--- app/Views/admin/alumini/alumini-page-notification.php
+++ app/Views/admin/alumini/alumini-page-notification.php
@@ -9,30 +9,44 @@
 </style>
 <!-- start page title -->
 <div class="row">
-    <div class="col-md-5">
+    <div class="col-md-4">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Add <?= $title ?></h4>
             </div>
-            <div class="card-body">
+            <form action="<?= base_url() ?>admin/alumini-page-notification" method="post" enctype="multipart/form-data" id="alumini-page-notification-form">
+            <div class="card-body p-1">
                 <?php if(session()->getFlashdata('status')){
                         echo session()->getFlashdata('status');
                 } ?>
-            <form action="<?= base_url() ?>admin/alumini-page-notification" method="post">
                 <div class="form-group">
-                    <label for="title">Title</label>
+                    <span for="title">Title</span>
                     <input type="text" class="form-control" name="title" id="title" placeholder="Enter Title" required>
                 </div>
-            </form>
+                <div class="form-group">
+                    <span for="title">Description</span>
+                    <textarea name="description" id="editor"></textarea>
+                </div>
+                <div class="form-group">
+                    <span for="title">File Upload</span>
+                    <input type="file" class="form-control" name="file_upload" accept=".pdf" required>
+                </div>
+                <div class="form-group">
+                    <span for="title"><input type="checkbox" name="marquee-status"> Marquee Status</span>
+                </div>
             </div>
+            <div class="card-footer">
+                <input type="submit" class="btn btn-primary" value="Submit" id="submit">
+            </div>
+            </form>
         </div>
     </div>
-    <div class="col-md-7">
+    <div class="col-md-8">
         <div class="card">
             <div class="card-header">
-                <h4 class="card-title m-0">Add <?= $title ?></h4>
+                <h4 class="card-title m-0"><?= $title ?> List</h4>
             </div>
-            <div class="card-body">
+            <div class="card-body p-1">
                 
             </div>
         </div>
