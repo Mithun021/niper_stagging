f2ceba182c0f58c73f4a3ce8c6929e83fa5694cc
Mithun021 <mithunkr79038@gmail.com>
2025-04-30 11:07:16 +0530

    student dashboard

--- app/Views/admin/alumini/alumini-page-notification.php
+++ app/Views/admin/alumini/alumini-page-notification.php
@@ -9,17 +9,31 @@
 </style>
 <!-- start page title -->
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-5">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Add <?= $title ?></h4>
             </div>
             <div class="card-body">
-                <?php
-                    if(session()->getFlashdata('status')){
+                <?php if(session()->getFlashdata('status')){
                         echo session()->getFlashdata('status');
-                    }
-                ?>
+                } ?>
+            <form action="<?= base_url() ?>admin/alumini-page-notification" method="post">
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" name="title" id="title" placeholder="Enter Title" required>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-7">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title m-0">Add <?= $title ?></h4>
+            </div>
+            <div class="card-body">
+                
             </div>
         </div>
     </div>
