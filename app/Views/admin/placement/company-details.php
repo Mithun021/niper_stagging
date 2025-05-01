fd0c378c72d8c452778e8682e84acb4d65c7a60d
Mithun021 <mithunkr79038@gmail.com>
2025-05-01 11:50:51 +0530

    student dashboard

--- app/Views/admin/placement/company-details.php
+++ app/Views/admin/placement/company-details.php
@@ -1,11 +1,13 @@
 <?= $this->extend("admin/layouts/master") ?>
-<?=  $this->section("body-content"); ?>
+<?= $this->section("body-content"); ?>
 <?php
-    use App\Models\Employee_model;
-    $employee_model = new Employee_model();
+
+use App\Models\Employee_model;
+
+$employee_model = new Employee_model();
 ?>
 <style>
-    
+
 </style>
 <!-- start page title -->
 <div class="row">
@@ -19,57 +21,59 @@
                     <?php if (session()->getFlashdata('status')) {
                         echo session()->getFlashdata('status');
                     } ?>
-                    <div class="form-group col-md-6">
-                        <span for="title">Company Name</span>
-                        <input type="text" class="form-control form-control-sm" name="copanty_name" id="copanty_name" placeholder="Enter Company Name" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <span for="title">Company Logo (jpeg, png, 500kb)</span>
-                        <input type="file" class="form-control form-control-sm" name="company_logo" accept=".jpg,.png,.jpeg" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <span for="title">Company Photo (jpeg, png, 500kb)</span>
-                        <input type="file" class="form-control form-control-sm" name="company_photo" accept=".jpg,.png,.jpeg">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <span for="title">Company Profile</span>
-                        <input type="text" name="company_profile" class="form-control form-control-sm" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <span for="title">Company Website link</span>
-                        <input type="url" name="company_website" class="form-control form-control-sm" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <span for="title">Linkedin</span>
-                        <input type="url" name="linkedin" class="form-control form-control-sm" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <span for="title">Facebook</span>
-                        <input type="url" name="facebook" class="form-control form-control-sm" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <span for="title">Instagram</span>
-                        <input type="url" name="instagram" class="form-control form-control-sm" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <span for="title">Twitter</span>
-                        <input type="url" name="twitter" class="form-control form-control-sm" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <span for="title">Email 1</span>
-                        <input type="email" name="email_1" class="form-control form-control-sm" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <span for="title">Email 2</span>
-                        <input type="email" name="email_2" class="form-control form-control-sm" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <span for="title">Helpline number 1</span>
-                        <input type="tel" name="helpline_number1" class="form-control form-control-sm" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <span for="title">Helpline number 2</span>
-                        <input type="tel" name="helpline_number2" class="form-control form-control-sm" required>
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <span for="title">Company Name</span>
+                            <input type="text" class="form-control form-control-sm" name="copanty_name" id="copanty_name" placeholder="Enter Company Name" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="title">Company Logo (jpeg, png, 500kb)</span>
+                            <input type="file" class="form-control form-control-sm" name="company_logo" accept=".jpg,.png,.jpeg" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="title">Company Photo (jpeg, png, 500kb)</span>
+                            <input type="file" class="form-control form-control-sm" name="company_photo" accept=".jpg,.png,.jpeg">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="title">Company Profile</span>
+                            <input type="text" name="company_profile" class="form-control form-control-sm" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="title">Company Website link</span>
+                            <input type="url" name="company_website" class="form-control form-control-sm" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="title">Linkedin</span>
+                            <input type="url" name="linkedin" class="form-control form-control-sm" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="title">Facebook</span>
+                            <input type="url" name="facebook" class="form-control form-control-sm" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="title">Instagram</span>
+                            <input type="url" name="instagram" class="form-control form-control-sm" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="title">Twitter</span>
+                            <input type="url" name="twitter" class="form-control form-control-sm" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="title">Email 1</span>
+                            <input type="email" name="email_1" class="form-control form-control-sm" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="title">Email 2</span>
+                            <input type="email" name="email_2" class="form-control form-control-sm" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="title">Helpline number 1</span>
+                            <input type="tel" name="helpline_number1" class="form-control form-control-sm" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <span for="title">Helpline number 2</span>
+                            <input type="tel" name="helpline_number2" class="form-control form-control-sm" required>
+                        </div>
                     </div>
                 </div>
                 <div class="card-footer p-2">
