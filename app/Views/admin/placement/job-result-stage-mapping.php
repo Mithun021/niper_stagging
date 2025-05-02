d80a5549e6dae292ca0cb5b837b5c6a06a686ac6
Mithun021 <mithunkr79038@gmail.com>
2025-05-02 12:54:09 +0530

    first commit

--- app/Views/admin/placement/job-result-stage-mapping.php
+++ app/Views/admin/placement/job-result-stage-mapping.php
@@ -1,25 +1,37 @@
 <?= $this->extend("admin/layouts/master") ?>
-<?=  $this->section("body-content"); ?>
+<?= $this->section("body-content"); ?>
 <?php
-    use App\Models\Employee_model;
-    $employee_model = new Employee_model();
+
+use App\Models\Employee_model;
+
+$employee_model = new Employee_model();
 ?>
 <style>
-    
+
 </style>
 <!-- start page title -->
 <div class="row">
-    <div class="col-lg-12">
+    <div class="col-lg-4">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title m-0">Add <?= $title ?></h4>
             </div>
-            <div class="card-body">
-                <?php
-                    if(session()->getFlashdata('status')){
+            <form action="<?= base_url() ?>admin/job-result-stage-mapping" method="post" enctype="multipart/form-data" id="alumini-page-notification-form">
+                <div class="card-body p-1">
+                    <?php if (session()->getFlashdata('status')) {
                         echo session()->getFlashdata('status');
-                    }
-                ?>
+                    } ?>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="col-lg-8">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title m-0"><?= $title ?> List</h4>
+            </div>
+            <div class="card-body p-1">
+
             </div>
         </div>
     </div>
