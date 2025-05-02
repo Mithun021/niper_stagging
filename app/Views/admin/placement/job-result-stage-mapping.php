87b4afbe1a7a07f6743f3abc2127f5213a1b2e6f
Mithun021 <mithunkr79038@gmail.com>
2025-05-02 13:21:15 +0530

    first commit

--- app/Views/admin/placement/job-result-stage-mapping.php
+++ app/Views/admin/placement/job-result-stage-mapping.php
@@ -1,10 +1,10 @@
 <?= $this->extend("admin/layouts/master") ?>
 <?= $this->section("body-content"); ?>
 <?php
-
 use App\Models\Employee_model;
-
+use App\Models\Placement_job_details_model;
 $employee_model = new Employee_model();
+$placement_job_result_model = new Placement_job_details_model();
 ?>
 <style>
 
@@ -39,6 +39,9 @@ $employee_model = new Employee_model();
                         <textarea name="result_description" id="editor" class="form-control form-control-sm"></textarea>
                     </div>
                 </div>
+                <div class="card-footer p-2">
+                    <input type="submit" class="btn btn-primary" value="Submit" id="submit">
+                </div>
             </form>
         </div>
     </div>
@@ -48,7 +51,38 @@ $employee_model = new Employee_model();
                 <h4 class="card-title m-0"><?= $title ?> List</h4>
             </div>
             <div class="card-body p-1">
-
+            <div class="table-responsive">
+                    <table class="table table-striped table-hover" id="basic-datatable">
+                        <thead>
+                            <tr>
+                                <td>SN</td>
+                                <td>Job id</td>
+                                <td>Result Title</td>
+                                <td>Result Description</td>
+                                <td>Upload by</td>
+                                <td>Action</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($job_result_stage_mapping as $key => $value) { ?>
+                                <tr>
+                                    <td><?= $placement_job_result_model->get($value['job_id'])['job_title'] ?? '' ?></td>
+                                    <td><?= $value['result_title'] ?></td>
+                                    <td><?= $value['result_description'] ?></td>
+                                    <td><?php $emp = $employee_model->get($value['upload_by']); if($emp){
+                                        echo $emp['first_name'] . " " . $emp['middle_name'] . " " . $emp['last_name']; }  ?></td>
+                                    <td>
+                                        <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
+                                            <!-- <a href="#" class="btn btn-dark waves-effect waves-light"><i class="far fa-eye"></i></a> -->
+                                            <a href="#" class="btn btn-primary waves-effect waves-light"><i class="fas fa-pen"></i></a>
+                                            <a href="<?= base_url() ?>admin/delete-result-details/<?= $value['id'] ?>" class="btn btn-danger waves-effect waves-light" onclick="return confirm('Are you sure..!')"><i class="far fa-trash-alt"></i></a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
