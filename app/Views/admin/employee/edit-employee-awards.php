d3b0872a6000660a7973fa1cb639e9594fe5824b
Mithun021 <mithunkr79038@gmail.com>
2025-05-10 15:02:17 +0530

    first commit

--- app/Views/admin/employee/edit-employee-awards.php
+++ app/Views/admin/employee/edit-employee-awards.php
@@ -139,8 +139,8 @@ $employee_model = new Employee_model();
                                     <td>
                                         <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
                                             <!-- <a href="#" class="btn btn-dark waves-effect waves-light"><i class="far fa-eye"></i></a> -->
-                                            <a href="<?= base_url() ?>edit-employee-awards/<?= $value['id'] ?>" class="btn btn-primary waves-effect waves-light"><i class="fas fa-pen"></i></a>
-                                            <a href="<?= base_url() ?>delete-employee-awards/<?= $value['id'] ?>" class="btn btn-danger waves-effect waves-light"><i class="far fa-trash-alt"></i></a>
+                                            <a href="<?= base_url() ?>admin/edit-employee-awards/<?= $value['id'] ?>" class="btn btn-primary waves-effect waves-light"><i class="fas fa-pen"></i></a>
+                                            <a href="<?= base_url() ?>admin/delete-employee-awards/<?= $value['id'] ?>" class="btn btn-danger waves-effect waves-light"><i class="far fa-trash-alt"></i></a>
                                         </div>
                                     </td>
                                 </tr>
