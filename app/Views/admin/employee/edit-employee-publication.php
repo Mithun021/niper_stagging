bb0032beef9cad2fc506eb0874696a070eb4c495
Mithun021 <mithunkr79038@gmail.com>
2025-05-10 14:12:54 +0530

    first commit

--- app/Views/admin/employee/edit-employee-publication.php
+++ app/Views/admin/employee/edit-employee-publication.php
@@ -343,7 +343,7 @@
 
     function deletePubAuthor(id){
         if(confirm('Are you sure...!')){
-            alert(id); return false;
+            // alert(id); return false;
             $.ajax({
                 type: "GET",
                 url: "<?= base_url() ?>admin/deletPubAuthor/" + id,
